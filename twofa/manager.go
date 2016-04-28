2951e6bdaa288bd76a0e389325cd6a4cd18ef918
aeneasr <aeneas.rekkas@serlo.org>
2016-04-28 19:11:56 +0200

    unstaged

---
+++ twofa/manager.go
@@ -0,0 +1,30 @@
+package twofa
+
+import (
+	"github.com/go-errors/errors"
+	"github.com/pquerna/otp/totp"
+)
+
+type Manager interface {
+	Generate(subject string) error
+	Validate(subject string) error
+}
+
+type TOTP struct {
+	Issuer string
+	Period uint
+}
+
+func (m *TOTP) Generate(subject string) error {
+	key, err := totp.Generate(&totp.GenerateOpts{
+		// Name of the issuing Organization/Company.
+		Issuer: m.Issuer,
+		// Name of the User's Account (eg, email address)
+		AccountName: subject,
+		// Number of seconds a TOTP hash is valid for. Defaults to 30 seconds.
+		Period: m.Period,
+	})
+	if err != nil {
+		return errors.New(err)
+	}
+}

3a392cd4dc420749e9b27d976a00bbf8a1d381b5
Mohamedh Fazal <mohamedhfazal@gmail.com>
2016-06-30 14:41:33 +0500

    added a thin wrapper for HTTP Manager's to be used as a HTTP client library.

---
+++ hydra/hydra_opts.go
@@ -0,0 +1,75 @@
+package hydra
+
+import (
+	"io/ioutil"
+	"net/url"
+
+	"gopkg.in/yaml.v1"
+)
+
+func ClusterURL(urlStr string) option {
+	return func(c *Hydra) error {
+		var err error
+		c.clusterURL, err = url.Parse(urlStr)
+		return err
+	}
+}
+
+func FromYAML(file string) option {
+	return func(c *Hydra) error {
+		var err error
+		var config = struct {
+			ClusterURL   string `yaml:"cluster_url"`
+			ClientID     string `yaml:"client_id"`
+			ClientSecret string `yaml:"client_secret"`
+		}{}
+
+		data, err := ioutil.ReadFile(file)
+		if err != nil {
+			return err
+		}
+
+		err = yaml.Unmarshal(data, &config)
+		if err != nil {
+			return err
+		}
+
+		c.clusterURL, err = url.Parse(config.ClusterURL)
+		if err != nil {
+			return err
+		}
+
+		c.clientID = config.ClientID
+		c.clientSecret = config.ClientSecret
+
+		return nil
+	}
+}
+
+func ClientID(id string) option {
+	return func(c *Hydra) error {
+		c.clientID = id
+		return nil
+	}
+}
+
+func ClientSecret(secret string) option {
+	return func(c *Hydra) error {
+		c.clientSecret = secret
+		return nil
+	}
+}
+
+func SkipSSL() option {
+	return func(c *Hydra) error {
+		c.skipSSL = true
+		return nil
+	}
+}
+
+func Scopes(scopes ...string) option {
+	return func(c *Hydra) error {
+		c.scopes = scopes
+		return nil
+	}
+}

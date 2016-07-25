a62a2f3599c58b11dd36ff8589a48955736271d8
Aeneas Rekkas (arekkas) <aeneas@ory.am>
2016-07-25 14:20:43 +0200

    firewall: document warden interface sdk

--- firewall/warden.go
+++ firewall/warden.go
@@ -18,9 +18,19 @@ type Context struct {
 }
 
 type Firewall interface {
+	// Authorized checks if the given token is valid and if the requested scopes are satisfied and returns
+	// a context if yes and an error if no.
 	Authorized(ctx context.Context, token string, scopes ...string) (*Context, error)
+
+	// HTTPAuthorized checks if the given HTTP request is authorized and if the requested scopes are satisfied and returns
+	// a context if yes and an error if no.
 	HTTPAuthorized(ctx context.Context, r *http.Request, scopes ...string) (*Context, error)
 
+	// ActionAllowed, apart from doing the same thing as Authorized, checks if the token's subject is allowed to perform
+	// the given action on the given resource. Returns an error if any of the parameters are not fulfilled.
 	ActionAllowed(ctx context.Context, token string, accessRequest *ladon.Request, scopes ...string) (*Context, error)
+
+	// HTTPActionAllowed, apart from doing the same thing as Authorized, checks if the token's subject (extracted from the HTTP Request) is allowed to perform
+	// the given action on the given resource. Returns an error if any of the parameters are not fulfilled.
 	HTTPActionAllowed(ctx context.Context, r *http.Request, accessRequest *ladon.Request, scopes ...string) (*Context, error)
 }

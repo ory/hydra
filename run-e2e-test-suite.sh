35523a298e1e6cc53843468bb1685d89a1907855
Aeneas Rekkas (arekkas) <aeneas@ory.am>
2017-01-02 21:23:15 +0100

    e2e: mark script executable

--- run-e2e-test-suite.sh
+++ run-e2e-test-suite.sh
@@ -2,10 +2,6 @@
 
 export PATH=PATH:$GOPATH/bin
 
-# Boot hydra
-hydra host --dangerous-auto-logon --dangrous-force-http &
-while ! echo exit | nc localhost 4444; do sleep 1; done
-
 # Test clients
 hydra clients create --id foobar
 hydra clients delete foobar

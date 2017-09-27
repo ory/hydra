8cd73da2517c84163d47745ad2e98a74b95a9dd9
arekkas <aeneas@ory.am>
2017-09-27 11:16:29 +0200

    scripts: remove swagger-codegen jar from git

--- scripts/run-gensdk.sh
+++ scripts/run-gensdk.sh
@@ -4,6 +4,8 @@ set -euo pipefail
 
 cd "$( dirname "${BASH_SOURCE[0]}" )/.."
 
+curl -O scripts/swagger-codegen-cli-2.2.3.jar http://central.maven.org/maven2/io/swagger/swagger-codegen-cli/2.2.3/swagger-codegen-cli-2.2.3.jar
+
 java -jar scripts/swagger-codegen-cli-2.2.3.jar generate -i ./docs/api.swagger.json -l go -o ./sdk/go/swagger
 java -jar scripts/swagger-codegen-cli-2.2.3.jar generate -i ./docs/api.swagger.json -l javascript -o ./sdk/js/swagger
 

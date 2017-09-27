f792d1b5df5541c29330c3e9d44363a9f87d5380
arekkas <aeneas@ory.am>
2017-09-27 16:48:58 +0200

    sdk: move sdk one directory down

--- scripts/run-gensdk.sh
+++ scripts/run-gensdk.sh
@@ -6,16 +6,16 @@ cd "$( dirname "${BASH_SOURCE[0]}" )/.."
 
 scripts/run-genswag.sh
 
-rm -rf ./sdk/go/swagger
+rm -rf ./sdk/go/hydra/swagger
 
 # curl -O scripts/swagger-codegen-cli-2.2.3.jar http://central.maven.org/maven2/io/swagger/swagger-codegen-cli/2.2.3/swagger-codegen-cli-2.2.3.jar
 
-java -jar scripts/swagger-codegen-cli-2.2.3.jar generate -i ./docs/api.swagger.json -l go -o ./sdk/go/swagger
+java -jar scripts/swagger-codegen-cli-2.2.3.jar generate -i ./docs/api.swagger.json -l go -o ./sdk/go/hydra/swagger
 java -jar scripts/swagger-codegen-cli-2.2.3.jar generate -i ./docs/api.swagger.json -l javascript -o ./sdk/js/swagger
 
 scripts/run-format.sh
 
 git add -A .
 
-git checkout HEAD -- sdk/go/swagger/configuration.go
-git checkout HEAD -- sdk/go/swagger/api_client.go
+git checkout HEAD -- sdk/go/hydra/swagger/configuration.go
+git checkout HEAD -- sdk/go/hydra/swagger/api_client.go

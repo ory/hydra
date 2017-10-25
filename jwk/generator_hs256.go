aff81373caa5f4d5c1a0ef6320aad5bb50b5ec52
arekkas <aeneas@ory.am>
2017-10-25 10:45:21 +0200

    jwk: use cryptopasta library

--- jwk/generator_hs256.go
+++ jwk/generator_hs256.go
@@ -2,35 +2,31 @@ package jwk
 
 import (
 	"crypto/x509"
-
-	"github.com/ory/hydra/rand/sequence"
 	"github.com/pkg/errors"
 	"github.com/square/go-jose"
+	"io"
+	"crypto/rand"
 )
 
 type HS256Generator struct {
-	Length int
 }
 
 func (g *HS256Generator) Generate(id string) (*jose.JsonWebKeySet, error) {
-	if g.Length < 12 {
-		g.Length = 12
-	}
-
-	if id == "" {
-		id = "shared"
-	}
-
-	key, err := sequence.RuneSequence(g.Length, []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789,.-;:_#+*!§$%&/()=?}][{<>"))
+	// Taken from NewHMACKey
+	key := &[32]byte{}
+	_, err := io.ReadFull(rand.Reader, key[:])
 	if err != nil {
-		return nil, errors.Errorf("Could not generate key because %s", err)
+		return nil, errors.WithStack(err)
 	}
+
+	var sliceKey []byte
+	copy(sliceKey, key[:])
 
 	return &jose.JsonWebKeySet{
 		Keys: []jose.JsonWebKey{
 			{
 				Algorithm:    "HS256",
-				Key:          []byte(string(key)),
+				Key:          sliceKey,
 				KeyID:        id,
 				Certificates: []*x509.Certificate{},
 			},

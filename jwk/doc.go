dcbd6d8fcb32a643732ad97656d35e4eda2ddfaf
Aeneas <arekkas@users.noreply.github.com>
2017-11-06 13:01:53 +0100

    Add license header to all source files (#644)
    Closes #643

--- jwk/doc.go
+++ jwk/doc.go
@@ -4,6 +4,20 @@
 // structure that represents a cryptographic key. A JWK Set is a JSON data structure that
 // represents a set of JWKs. A JSON Web Key is identified by its set and key id. ORY Hydra uses this functionality
 // to store cryptographic keys used for TLS and JSON Web Tokens (such as OpenID Connect ID tokens).
+// Copyright © 2017 Aeneas Rekkas <aeneas+oss@aeneas.io>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package jwk
 
 // swagger:parameters getJsonWebKey deleteJsonWebKey

32259b09c2cdf2c907e2baad49b7eed7ab6ad932
aeneasr <aeneas.rekkas@serlo.org>
2017-12-07 18:23:28 +0100

    unstaged

--- pkg/errors_test.go
+++ pkg/errors_test.go
@@ -10,6 +10,16 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+type errStackTracer struct{}
+
+func (s *errStackTracer) StackTrace() errors.StackTrace {
+	return errors.StackTrace{}
+}
+
+func (s *errStackTracer) Error() string {
+	return "foo"
+}
+
 func TestLogError(t *testing.T) {
 	buf := bytes.NewBuffer([]byte{})
 	l := logrus.New()
@@ -20,6 +30,8 @@ func TestLogError(t *testing.T) {
 	t.Logf("%s", string(buf.Bytes()))
 
 	assert.True(t, strings.Contains(string(buf.Bytes()), "Stack trace"))
+
+	LogError(new(errStackTracer), l)
 }
 
 func TestLogErrorDoesNotPanic(t *testing.T) {

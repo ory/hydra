fabee0dcafa4056802f9e88492e268e114f03f9d
arekkas <aeneas@ory.am>
2017-12-09 17:57:01 +0100

    oauth2: Adds userinfo endpoint and improves OIDC discovery

--- sdk/go/hydra/swagger/handler.go
+++ sdk/go/hydra/swagger/handler.go
@@ -11,8 +11,6 @@
 package swagger
 
 type Handler struct {
-	Generators map[string]KeyGenerator `json:"Generators,omitempty"`
-
 	H Writer `json:"H,omitempty"`
 
 	Manager Manager `json:"Manager,omitempty"`

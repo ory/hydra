2ad0a96503531ff16c10d174454b37d49d30c8b4
arekkas <aeneas@ory.am>
2018-02-18 19:03:54 +0100

    docs: Clean up swagger specification

--- doc.go
+++ doc.go
@@ -1,15 +1,8 @@
 // Package main Hydra OAuth2 & OpenID Connect Server
 //
-// Please refer to the user guide for in-depth documentation: https://www.ory.sh/docs
-//
-//
 // Hydra offers OAuth 2.0 and OpenID Connect Core 1.0 capabilities as a service. Hydra is different, because it works with any existing authentication infrastructure, not just LDAP or SAML. By implementing a consent app (works with any programming language) you build a bridge between Hydra and your authentication infrastructure.
 // Hydra is able to securely manage JSON Web Keys, and has a sophisticated policy-based access control you can use if you want to.
 // Hydra is suitable for green- (new) and brownfield (existing) projects. If you are not familiar with OAuth 2.0 and are working on a greenfield project, we recommend evaluating if OAuth 2.0 really serves your purpose. Knowledge of OAuth 2.0 is imperative in understanding what Hydra does and how it works.
-//
-//
-// The official repository is located at https://github.com/ory/hydra
-//
 //
 // The API documentation always refers to the latest tagged version of ORY Hydra. For previous API documentations, please
 // refer to `https://github.com/ory/hydra/blob/<tag-id>/docs/api.swagger.yaml` - for example:

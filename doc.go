6cbfa58c6552f77ef4acd1cb949fbda6e5a69189
arekkas <aeneas@ory.am>
2018-02-18 18:34:44 +0100

    docs: Removes apiary how-to

--- doc.go
+++ doc.go
@@ -1,6 +1,6 @@
 // Package main Hydra OAuth2 & OpenID Connect Server
 //
-// Please refer to the user guide for in-depth documentation: https://ory.gitbooks.io/hydra/content/
+// Please refer to the user guide for in-depth documentation: https://www.ory.sh/docs
 //
 //
 // Hydra offers OAuth 2.0 and OpenID Connect Core 1.0 capabilities as a service. Hydra is different, because it works with any existing authentication infrastructure, not just LDAP or SAML. By implementing a consent app (works with any programming language) you build a bridge between Hydra and your authentication infrastructure.
@@ -11,15 +11,8 @@
 // The official repository is located at https://github.com/ory/hydra
 //
 //
-// ### Important REST API Documentation Notes
-//
-// The swagger generator used to create this documentation does currently not support example responses. To see
-// request and response payloads click on **"Show JSON schema"**:
-// ![Enable JSON Schema on Apiary](https://storage.googleapis.com/ory.am/hydra/json-schema.png)
-//
-//
 // The API documentation always refers to the latest tagged version of ORY Hydra. For previous API documentations, please
-// refer to https://github.com/ory/hydra/blob/<tag-id>/docs/api.swagger.yaml - for example:
+// refer to `https://github.com/ory/hydra/blob/<tag-id>/docs/api.swagger.yaml` - for example:
 //
 // - 0.9.13: https://github.com/ory/hydra/blob/v0.9.13/docs/api.swagger.yaml
 // - 0.8.1: https://github.com/ory/hydra/blob/v0.8.1/docs/api.swagger.yaml
@@ -29,7 +22,7 @@
 //     BasePath: /
 //     Version: Latest
 //     License: Apache 2.0 https://github.com/ory/hydra/blob/master/LICENSE
-//     Contact: ORY <hi@ory.am> https://www.ory.am
+//     Contact: ORY <hi@ory.am> https://www.ory.sh
 //
 //     Consumes:
 //     - application/json

73762c6f9da74a9590423269349decd5b09bce32
arekkas <aeneas@ory.am>
2018-04-29 18:09:15 +0200

    build: Updates to Go 1.10

--- oauth2/session.go
+++ oauth2/session.go
@@ -29,7 +29,7 @@ import (
 
 type Session struct {
 	*openid.DefaultSession `json:"idToken"`
-	Audience []string
+	Audience               []string
 	Extra                  map[string]interface{} `json:"extra"`
 }
 

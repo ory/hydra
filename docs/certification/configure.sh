ee81c200a8cf13b784afea5d36f91b5fdfcd044c
arekkas <aeneas@ory.am>
2018-05-20 16:27:42 +0200

    docs: Updates oidc-conformity proof and scripts

--- docs/certification/configure.sh
+++ docs/certification/configure.sh
@@ -10,7 +10,7 @@ hydra clients create \
     --endpoint http://localhost:9000 \
     --id test-client \
     --secret test-secret \
-    --response-types code,id_token \
-    --grant-types refresh_token,authorization_code \
+    --response-types code,id_token,token \
+    --grant-types implicit,refresh_token,authorization_code \
     --scope openid,offline,offline_access,profile,email,address,phone \
-    --callbacks https://op.certification.openid.net:60848/authz_cb
+    --callbacks https://op.certification.openid.net:60848/authz_cb,https://op.certification.openid.net:61290/authz_cb,https://op.certification.openid.net:61291/authz_cb

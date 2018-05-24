6369452c6896128f6609e3daa35ecab78ff19500
arekkas <aeneas@ory.am>
2018-05-24 12:15:51 +0200

    docs: Add redirect URIs for all flows to oidc scripts

--- docs/certification/configure.sh
+++ docs/certification/configure.sh
@@ -13,4 +13,4 @@ hydra clients create \
     --response-types code,id_token,token \
     --grant-types implicit,refresh_token,authorization_code \
     --scope openid,offline,offline_access,profile,email,address,phone \
-    --callbacks https://op.certification.openid.net:60848/authz_cb,https://op.certification.openid.net:61290/authz_cb,https://op.certification.openid.net:61291/authz_cb
+    --callbacks https://op.certification.openid.net:60848/authz_cb,https://op.certification.openid.net:61290/authz_cb,https://op.certification.openid.net:61291/authz_cb,https://op.certification.openid.net:61302/authz_cb,https://op.certification.openid.net:61303/authz_cb,https://op.certification.openid.net:61304/authz_cb

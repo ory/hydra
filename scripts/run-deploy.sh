ec2f3d34218a0dd8d9c2c6bb0c65cb3e40397abc
arekkas <aeneas@ory.am>
2018-05-29 15:39:34 +0200

    ci: Improves build toolchain

--- scripts/run-deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-gox -ldflags "-X github.com/ory/hydra/cmd.Version=`git describe --tags` -X github.com/ory/hydra/cmd.BuildTime=`TZ=UTC date -u '+%Y-%m-%dT%H:%M:%SZ'` -X github.com/ory/hydra/cmd.GitHash=`git rev-parse HEAD`" -output "dist/{{.Dir}}-{{.OS}}-{{.Arch}}";
-npm version -f --no-git-tag-version $(git describe --tag);

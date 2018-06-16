ad86dd18d3998ba022d3979a7be4d620ca399df3
arekkas <aeneas@ory.am>
2018-06-16 11:45:21 +0200

    oauth2: Implements dynamic client registration

--- docs/certification/start.sh
+++ docs/certification/start.sh
@@ -6,6 +6,33 @@ domain=oidc-certification.ory.sh:8443
 hydraport=9000
 idport=9001
 
+docker start kong-database
+docker start kong
+
+dockerize -wait http://localhost:8001/ -timeout 30s
+
+ip=$(curl ifconfig.co)
+
+curl -i -X DELETE --url http://localhost:8001/apis/hydra-oauth
+curl -i -X DELETE --url http://localhost:8001/apis/login-consent
+
+curl -i -X POST \
+  --url http://localhost:8001/apis/ \
+  --data 'name=hydra-oauth' \
+  --data upstream_url=http://${ip}:9000/ \
+  --data 'uris=/oauth2,/.well-known,/userinfo,/clients' \
+  --data 'strip_uri=false' \
+  --data 'preserve_host=true'
+
+curl -i -X POST \
+  --url http://localhost:8001/apis/ \
+  --data 'name=login-consent' \
+  --data upstream_url=http://$ip:9001/ \
+  --data 'uris=/login,/consent' \
+  --data 'strip_uri=false' \
+  --data 'preserve_host=true'
+
+
 (cd ./hydra-login-consent-node; HYDRA_URL=http://localhost:$hydraport PORT=$idport npm start &)
 
 PORT=$hydraport \

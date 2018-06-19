6837046546f384838c1d1a3324b23c609211baad
aeneasr <aeneas.rekkas@serlo.org>
2018-06-19 08:37:29 +0200

    oauth2: Declares grant type refresh_token as supported

--- oauth2/handler.go
+++ oauth2/handler.go
@@ -208,7 +208,7 @@ func (h *Handler) WellKnownHandler(w http.ResponseWriter, r *http.Request, _ htt
 		UserinfoEndpoint:                  userInfoEndpoint,
 		TokenEndpointAuthMethodsSupported: []string{"client_secret_post", "client_secret_basic", "private_key_jwt", "none"},
 		IDTokenSigningAlgValuesSupported:  []string{"RS256"},
-		GrantTypesSupported:               []string{"authorization_code", "implicit", "client_credentials"},
+		GrantTypesSupported:               []string{"authorization_code", "implicit", "client_credentials", "refresh_token"},
 		ResponseModesSupported:            []string{"query", "fragment"},
 		RequestParameterSupported:         true,
 		RequestURIParameterSupported:      true,

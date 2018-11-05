81769e41243eff363737c2c29178f5a0b56e6415
aeneasr <aeneas@ory.sh>
2018-11-05 13:13:57 +0100

    u

--- consent/manager_memory.go
+++ consent/manager_memory.go
@@ -32,6 +32,7 @@ import (
 	"github.com/ory/x/pagination"
 )
 
+
 type MemoryManager struct {
 	consentRequests        map[string]ConsentRequest
 	handledConsentRequests map[string]HandledConsentRequest

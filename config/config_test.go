6ced127cf5977bc6d6cb0760c15328799e87bf72
Shota SAWADA <xiootas@gmail.com>
2019-02-14 12:00:00 +0900

    config: Add comment of case meaning to TestDoesRequestSatisfyTermination and sort cases
    Signed-off-by: Shota SAWADA <xiootas@gmail.com>

--- config/config_test.go
+++ config/config_test.go
@@ -43,12 +43,24 @@ func TestDoesRequestSatisfyTermination(t *testing.T) {
 	assert.Error(t, c.DoesRequestSatisfyTermination(&http.Request{Header: http.Header{}, URL: new(url.URL)}))
 
 	c = &Config{AllowTLSTermination: "127.0.0.1/24"}
+
+	// case of no X-Forwarded-Proto header
 	r := &http.Request{Header: http.Header{}, URL: new(url.URL)}
 	assert.Error(t, c.DoesRequestSatisfyTermination(r))
 
+	// case that X-Forwarded-Proto is http
 	r = &http.Request{Header: http.Header{"X-Forwarded-Proto": []string{"http"}}, URL: new(url.URL)}
 	assert.Error(t, c.DoesRequestSatisfyTermination(r))
 
+	// case that invalid remote is out of configured CIDER
+	r = &http.Request{
+		RemoteAddr: "227.0.0.1:123",
+		Header:     http.Header{"X-Forwarded-Proto": []string{"https"}},
+		URL:        new(url.URL),
+	}
+	assert.Error(t, c.DoesRequestSatisfyTermination(r))
+
+	// case that remote address and X-Forwarded-For are out of configured CIDER
 	r = &http.Request{
 		RemoteAddr: "227.0.0.1:123",
 		Header: http.Header{
@@ -58,13 +70,7 @@ func TestDoesRequestSatisfyTermination(t *testing.T) {
 		URL: new(url.URL)}
 	assert.Error(t, c.DoesRequestSatisfyTermination(r))
 
-	r = &http.Request{
-		RemoteAddr: "227.0.0.1:123",
-		Header:     http.Header{"X-Forwarded-Proto": []string{"https"}},
-		URL:        new(url.URL),
-	}
-	assert.Error(t, c.DoesRequestSatisfyTermination(r))
-
+	// case that remote address is in range of configured CIDER
 	r = &http.Request{
 		RemoteAddr: "127.0.0.1:123",
 		Header:     http.Header{"X-Forwarded-Proto": []string{"https"}},
@@ -72,6 +78,7 @@ func TestDoesRequestSatisfyTermination(t *testing.T) {
 	}
 	assert.NoError(t, c.DoesRequestSatisfyTermination(r))
 
+	// case of same as above but requesting /health endpoint
 	r = &http.Request{
 		RemoteAddr: "127.0.0.1:123",
 		Header:     http.Header{"X-Forwarded-Proto": []string{"https"}},
@@ -79,13 +86,14 @@ func TestDoesRequestSatisfyTermination(t *testing.T) {
 	}
 	assert.NoError(t, c.DoesRequestSatisfyTermination(r))
 
+	// case that remote address is out of configured CIDER but X-Forwarded-For is in the range
 	r = &http.Request{
 		RemoteAddr: "227.0.0.2:123",
 		Header: http.Header{
 			"X-Forwarded-Proto": []string{"https"},
 			"X-Forwarded-For":   []string{"227.0.0.1, 127.0.0.1, 227.0.0.2"},
 		},
-		URL: &url.URL{Path: "/health"},
+		URL: new(url.URL),
 	}
 	assert.NoError(t, c.DoesRequestSatisfyTermination(r))
 }

03a28c3e27138fc18675810b81b2b499d147da84
David López <not4rent@gmail.com>
2019-03-20 13:21:38 +0100

    docker: refactor docker-compose for cleanness and readability
    Reorganize/split docker-compose config between multiple files for cleanness and readability
    
    Signed-off-by: David López <not4rent@gmail.com>

---
+++ scripts/5min-tutorial.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+DB=${DB:-postgres}
+TRACING=${TRACING:-false}
+TWOC=${TWOC:-false}
+
+DC="docker-compose -f docker-compose.yml"
+if [[ $DB == "mysql" ]]; then
+    DC+=" -f docker-compose-mysql.yml"
+fi
+if [[ $TRACING == true ]]; then
+    DC+=" -f docker-compose-tracing.yml"
+fi
+if [[ $TWOC == true ]]; then
+    DC+=" -f docker-compose-twoc.yml"
+fi
+DC+=" up --build"
+
+$DC
+

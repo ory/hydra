0ccfc0096d995a4a6546e61f8fd459c021db66d0
aeneasr <aeneas@ory.sh>
2019-04-02 12:53:31 +0200

    Resolve sql testing race issues
    Signed-off-by: aeneasr <aeneas@ory.sh>

--- driver/registry_sql.go
+++ driver/registry_sql.go
@@ -81,23 +81,22 @@ func (m *RegistrySQL) DB() *sqlx.DB {
 func (m *RegistrySQL) CreateSchemas() (int, error) {
 	var total int
 
-	// Ensure dependencies exist
-	_, _, _, _ = m.ClientManager(), m.ConsentManager(), m.KeyManager(), m.OAuth2Storage()
-
+	m.Logger().Debugf("Applying %s SQL migrations...", m.db.DriverName())
 	for k, s := range []schemaCreator{
-		m.km.(schemaCreator),
-		m.cm.(schemaCreator),
-		m.com.(schemaCreator),
-		m.fs.(schemaCreator),
+		m.KeyManager().(schemaCreator),
+		m.ClientManager().(schemaCreator),
+		m.ConsentManager().(schemaCreator),
+		m.OAuth2Storage().(schemaCreator),
 	} {
-		m.Logger().Debugf("Applying SQL migrations for manager: %T (%d)", s, k)
+		m.Logger().Debugf("Applying %s SQL migrations for manager: %T (%d)", m.db.DriverName(), s, k)
 		if c, err := s.CreateSchemas(); err != nil {
 			return c, err
 		} else {
-			m.Logger().Debugf("Successfully applied %d SQL migrations for manager: %T (%d)", c, s, k)
+			m.Logger().Debugf("Successfully applied %d %s SQL migrations from manager: %T (%d)", c, m.db.DriverName(), s, k)
 			total += c
 		}
 	}
+	m.Logger().Debugf("Applied %d %s SQL migrations", total, m.db.DriverName())
 
 	return total, nil
 }

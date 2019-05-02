b5c25651221788370f78ebc18437aff3052118cc
aeneasr <aeneas@ory.sh>
2019-05-02 12:03:41 +0200

    config: Remove duplicates JWKS IDs from wellknown config
    Closes #1413
    
    Signed-off-by: aeneasr <aeneas@ory.sh>

--- jwk/handler_test.go
+++ jwk/handler_test.go
@@ -27,6 +27,10 @@ import (
 	"net/http/httptest"
 	"testing"
 
+	"github.com/spf13/viper"
+
+	"github.com/ory/hydra/driver/configuration"
+
 	"github.com/ory/hydra/x"
 
 	"github.com/ory/hydra/internal"
@@ -34,19 +38,19 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"gopkg.in/square/go-jose.v2"
-
-	. "github.com/ory/hydra/jwk"
 )
 
 func TestHandlerWellKnown(t *testing.T) {
 	conf := internal.NewConfigurationWithDefaults()
 	reg := internal.NewRegistry(conf)
 
+	viper.Set(configuration.ViperKeyWellKnownKeys, []string{x.OpenIDConnectKeyName, x.OpenIDConnectKeyName})
+
 	router := x.NewRouterPublic()
 	IDKS, _ := testGenerator.Generate("test-id", "sig")
 
 	h := reg.KeyHandler()
-	require.NoError(t, reg.KeyManager().AddKeySet(context.TODO(), IDTokenKeyName, IDKS))
+	require.NoError(t, reg.KeyManager().AddKeySet(context.TODO(), x.OpenIDConnectKeyName, IDKS))
 
 	h.SetRoutes(router.RouterAdmin(), router, func(h http.Handler) http.Handler {
 		return h
@@ -61,6 +65,8 @@ func TestHandlerWellKnown(t *testing.T) {
 	var known jose.JSONWebKeySet
 	err = json.NewDecoder(res.Body).Decode(&known)
 	require.NoError(t, err, "problem in decoding response")
+
+	require.Len(t, known.Keys, 1)
 
 	resp := known.Key("public:test-id")
 	require.NotNil(t, resp, "Could not find key public")

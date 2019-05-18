3545d6449400ad500c9cddab409492bb66c0a99e
aeneasr <aeneas@ory.sh>
2019-05-18 14:29:42 +0200

    ci: Update release pipeline

---
+++ .circleci/release_name.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+release=$(curl -s "https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/releases")
+tag=$(echo ${release} | jq -r ".[0].tag_name")
+tag_name=$(echo ${release} | jq -r ".[0].name")
+
+if [[ -n "$tag_name" ]]; then
+    echo "export RELEASE_NAME=$tag_name" >> $BASH_ENV
+else
+    echo "export RELEASE_NAME=$tag" >> $BASH_ENV
+fi

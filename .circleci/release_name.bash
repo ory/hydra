84d9cdf79c7962f40440b946fd233ec2b858bf81
hackerman <3372410+aeneasr@users.noreply.github.com>
2019-05-19 00:19:25 +0200

    ci: Resolve goreleaser issues (#1445)

--- .circleci/release_name.bash
+++ .circleci/release_name.bash
@@ -6,6 +6,8 @@ tag_name=$(echo ${release} | jq -r ".[0].name")
 
 if [[ -n "$tag_name" ]]; then
     echo "export RELEASE_NAME=$tag_name" >> $BASH_ENV
-else
+elif [[ -n "$tag" ]]; then
     echo "export RELEASE_NAME=$tag" >> $BASH_ENV
+else
+    echo "export RELEASE_NAME=$CIRCLE_SHA1" >> $BASH_ENV
 fi

4780a534470e1ca73a19480164ba3fb462851cfe
Henrik Feldt <henrik@haf.se>
2019-08-05 17:48:05 +0200

    Create doc.go
    Like this?

---
+++ metrics/prometheus/doc.go
@@ -0,0 +1,13 @@
+
+// Outputs Prometheus metrics
+//
+// swagger:route GET /metrics/prometheus public isInstanceAlive
+//
+// Get snapshot metrics from the Hydra service
+//
+//     Produces:
+//     - plain/text
+//
+//     Responses:
+//       200: promResponse
+func swaggerPublicPrometheus() {}

929ed009638f9e420ee313276cd64f5f7d864bef
Chris Pike <clp207@psu.edu>
2019-10-14 09:13:38 -0400

    client: make postgres/mysql consistent, change cockroach to alter table

--- client/migrations/sql/postgres/14.sql
+++ client/migrations/sql/postgres/14.sql
@@ -1,5 +1,9 @@
 -- +migrate Up
-ALTER TABLE hydra_client ADD metadata TEXT NOT NULL DEFAULT '{}';
+ALTER TABLE hydra_client ADD metadata TEXT NULL;
+
+UPDATE hydra_client SET metadata='{}';
+
+ALTER TABLE hydra_client ALTER COLUMN metadata SET NOT NULL;
 
 -- +migrate Down
 ALTER TABLE hydra_client DROP COLUMN metadata;

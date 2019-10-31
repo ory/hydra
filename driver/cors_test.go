5d7cf583def1c4961ef3eb27cfba8da3b9e2075c
Maurizio Dominici <m.dominici@domoticalabs.com>
2019-10-31 12:14:43 +0100

    possible #1615 solution: partial wildcard allowed-origins, with just protocol specified (for example "http://*") could use glob ** pattern in order to match any sequence of characters (even the separator).
    Signed-off-by: Aterocana <dominicimaurizio<at>gmail.com>

--- driver/cors_test.go
+++ driver/cors_test.go
@@ -28,12 +28,11 @@ import (
 	"testing"
 	"time"
 
-	"github.com/ory/viper"
-
 	"github.com/ory/fosite"
 	. "github.com/ory/hydra/driver"
 	"github.com/ory/hydra/internal"
 	"github.com/ory/hydra/oauth2"
+	"github.com/ory/viper"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -156,6 +155,17 @@ func TestOAuth2AwareCORSMiddleware(t *testing.T) {
 			code:         http.StatusNotImplemented,
 			header:       http.Header{"Origin": {"http://foobar.com"}, "Authorization": {"Bearer " + token}},
 			expectHeader: http.Header{"Access-Control-Allow-Credentials": []string{"true"}, "Access-Control-Allow-Origin": []string{"http://foobar.com"}, "Access-Control-Expose-Headers": []string{"Content-Type"}, "Vary": []string{"Origin"}},
+		},
+		{
+			d: "should accept any allowed specified origin protocol",
+			prep: func() {
+				r.ClientManager().CreateClient(context.Background(), &client.Client{ClientID: "foo-11", Secret: "bar", AllowedCORSOrigins: []string{"*"}})
+				viper.Set("serve.public.cors.enabled", true)
+				viper.Set("serve.public.cors.allowed_origins", []string{"http://*", "https://*"})
+			},
+			code:         http.StatusNotImplemented,
+			header:       http.Header{"Origin": {"http://foo.foobar.com"}, "Authorization": {"Basic Zm9vLTQ6YmFy"}},
+			expectHeader: http.Header{"Access-Control-Allow-Credentials": []string{"true"}, "Access-Control-Allow-Origin": []string{"http://foo.foobar.com"}, "Access-Control-Expose-Headers": []string{"Content-Type"}, "Vary": []string{"Origin"}},
 		},
 	} {
 		t.Run(fmt.Sprintf("case=%d/description=%s", k, tc.d), func(t *testing.T) {

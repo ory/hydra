223a7365b3f9238d735be3804abadbf10810b370
aeneasr <aeneas@ory.sh>
2020-04-06 19:24:55 +0200

    chore: update docusaurus template
    Signed-off-by: aeneasr <aeneas@ory.sh>

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -33,14 +33,19 @@ const links = [
   },
 ]
 
+let version = ['latest']
+
 if (fs.existsSync('./versions.json')) {
-  const version = require('./versions.json');
+  version = require('./versions.json');
   if (version && version.length > 0) {
     links.push({
       label: version[0],
       position: 'right',
       to: 'versions'
     });
+  }
+  if (version.length === 0) {
+    version = ['latest']
   }
 }
 
@@ -60,9 +65,9 @@ module.exports = {
     algolia: {
       apiKey: '8463c6ece843b377565726bb4ed325b0',
       indexName: 'ory',
-      // algoliaOptions: {
-      //   facetFilters: ['language:LANGUAGE', 'version:VERSION'],
-      // },
+      algoliaOptions: {
+        facetFilters: [`tags:${config.projectSlug}`, 'tags:ecosystem', `version:${version[0]}`],
+      },
     },
     navbar: {
       logo: {

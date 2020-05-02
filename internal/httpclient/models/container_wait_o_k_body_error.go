8b4f7742cb6ce770d9c806da73f0f4c639fda7fb
aeneasr <aeneas@ory.sh>
2020-05-02 08:29:03 +0000

    autogen(openapi): Regenerate swagger spec and internal client

--- internal/httpclient/models/container_wait_o_k_body_error.go
+++ internal/httpclient/models/container_wait_o_k_body_error.go
@@ -10,7 +10,7 @@ import (
 	"github.com/go-openapi/swag"
 )
 
-// ContainerWaitOKBodyError ContainerWaitOKBodyError container waiting error, if any
+// ContainerWaitOKBodyError ContainerWaitOKBodyError ContainerWaitOKBodyError container waiting error, if any
 //
 // swagger:model ContainerWaitOKBodyError
 type ContainerWaitOKBodyError struct {

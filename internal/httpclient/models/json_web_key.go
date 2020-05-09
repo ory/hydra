0bd0b0dd353935c2bb53dd4f58f9072d5702a89b
Patrik <zepatrik@users.noreply.github.com>
2020-05-09 12:34:06 +0200

    refactor: moved AskForConfirmation to ory/x/cmdx (#1848)
    Co-authored-by: aeneasr <aeneas@ory.sh>

--- internal/httpclient/models/json_web_key.go
+++ internal/httpclient/models/json_web_key.go
@@ -12,7 +12,7 @@ import (
 	"github.com/go-openapi/validate"
 )
 
-// JSONWebKey JSONWebKey It is important that this model object is named JSONWebKey for
+// JSONWebKey It is important that this model object is named JSONWebKey for
 // "swagger generate spec" to generate only on definition of a
 // JSONWebKey.
 //

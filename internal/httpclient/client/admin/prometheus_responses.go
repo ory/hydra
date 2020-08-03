989bc7c6cc2f15e7afc079c26242a5295cdce463
aeneasr <aeneas@ory.sh>
2020-08-03 10:08:35 +0000

    autogen(openapi): Regenerate swagger spec and internal client

--- internal/httpclient/client/admin/prometheus_responses.go
+++ internal/httpclient/client/admin/prometheus_responses.go
@@ -28,7 +28,7 @@ func (o *PrometheusReader) ReadResponse(response runtime.ClientResponse, consume
 		return result, nil
 
 	default:
-		return nil, runtime.NewAPIError("unknown error", response, response.Code())
+		return nil, runtime.NewAPIError("response status code does not match any response statuses defined for this endpoint in the swagger spec", response, response.Code())
 	}
 }
 

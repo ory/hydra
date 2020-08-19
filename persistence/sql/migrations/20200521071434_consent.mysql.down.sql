4bf2f876481efd0fc866a427287a0edeab525708
Imran Ismail <imran.codely@gmail.com>
2020-08-20 00:53:19 +0800

    fix: a typo in one of the down migration

--- persistence/sql/migrations/20200521071434_consent.mysql.down.sql
+++ persistence/sql/migrations/20200521071434_consent.mysql.down.sql
@@ -1,1 +1,1 @@
-DROP INDEX hydra_oauth2_consent_request ON hydra_oauth2_consent_request_client_id_subject_idx;
+DROP INDEX hydra_oauth2_consent_request_client_id_subject_idx ON hydra_oauth2_consent_request;

9a03d8f662f6c9e53a758129a1913090926cbdce
priyankatapar <priyanka.tapar@gmail.com>
2020-09-24 17:41:37 +0530

    CD-218194: Use base docker image, added builder dockerfile.

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,12 +5,12 @@
 # NOTE: Never use TEST_SAND_MIGRATE_DB in production
 if [ "$TEST_SAND_MIGRATE_DB" == 'true' ]; then
     echo "migrating db..."
-    LD_PRELOAD=/usr/lib/libfipsify.so PROCESS_NAME="sand migrate" /bin/sand migrate sql $DATABASE_URL
+    LD_PRELOAD=/usr/lib/libfipsify.so PROCESS_NAME="sand migrate" /usr/bin/sand migrate sql $DATABASE_URL
 fi
 echo "Starting sand..."
 # NOTE: Never use TEST_SAND_DANGEROUS_FORCE_HTTP in production
 if [ "$TEST_SAND_DANGEROUS_FORCE_HTTP" == 'true' ]; then
-    LD_PRELOAD=/usr/lib/libfipsify.so PROCESS_NAME="sand server" /bin/sand host --dangerous-force-http
+    LD_PRELOAD=/usr/lib/libfipsify.so PROCESS_NAME="sand server" /usr/bin/sand host --dangerous-force-http
 else
-    LD_PRELOAD=/usr/lib/libfipsify.so PROCESS_NAME="sand server" /bin/sand host
+    LD_PRELOAD=/usr/lib/libfipsify.so PROCESS_NAME="sand server" /usr/bin/sand host
 fi

3e197f7b889c65f59fa41e5665e2dd22ce46f368
aeneasr <aeneas@ory.sh>
2020-11-17 22:25:24 +0000

    autogen(openapi): Regenerate swagger spec and internal client

--- internal/httpclient/client/admin/get_login_request_responses.go
+++ internal/httpclient/client/admin/get_login_request_responses.go
@@ -55,7 +55,7 @@ func (o *GetLoginRequestReader) ReadResponse(response runtime.ClientResponse, co
 		return nil, result
 
 	default:
-		return nil, runtime.NewAPIError("unknown error", response, response.Code())
+		return nil, runtime.NewAPIError("response status code does not match any response statuses defined for this endpoint in the swagger spec", response, response.Code())
 	}
 }
 

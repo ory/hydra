94c937cfeb92e20aa96c2003b16998010a613e95
aeneasr <aeneas@ory.sh>
2020-11-17 22:25:24 +0000

    autogen(openapi): Regenerate swagger spec and internal client

--- internal/httpclient/client/public/is_instance_ready_responses.go
+++ internal/httpclient/client/public/is_instance_ready_responses.go
@@ -37,7 +37,7 @@ func (o *IsInstanceReadyReader) ReadResponse(response runtime.ClientResponse, co
 		return nil, result
 
 	default:
-		return nil, runtime.NewAPIError("unknown error", response, response.Code())
+		return nil, runtime.NewAPIError("response status code does not match any response statuses defined for this endpoint in the swagger spec", response, response.Code())
 	}
 }
 

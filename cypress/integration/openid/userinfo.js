f0641e4d630d112b7bde77c47122f47ae88923a4
aeneasr <3372410+aeneasr@users.noreply.github.com>
2020-11-17 14:19:02 +0100

    style: format cypress files

--- cypress/integration/openid/userinfo.js
+++ cypress/integration/openid/userinfo.js
@@ -1,4 +1,4 @@
-import { prng } from '../../helpers';
+import { prng } from '../../helpers'
 
 describe('OpenID Connect Userinfo', () => {
   const nc = () => ({
@@ -7,24 +7,24 @@ describe('OpenID Connect Userinfo', () => {
     scope: 'openid',
     redirect_uris: [`${Cypress.env('client_url')}/openid/callback`],
     grant_types: ['authorization_code', 'refresh_token']
-  });
+  })
 
   it('should return a proper userinfo response', function() {
-    const client = nc();
-    cy.authCodeFlow(client, { consent: { scope: ['openid'] } }, 'openid');
+    const client = nc()
+    cy.authCodeFlow(client, { consent: { scope: ['openid'] } }, 'openid')
 
     cy.get('body')
       .invoke('text')
       .then(content => {
-        const { result } = JSON.parse(content);
-        expect(result).to.equal('success');
-      });
+        const { result } = JSON.parse(content)
+        expect(result).to.equal('success')
+      })
 
     cy.request(`${Cypress.env('client_url')}/openid/userinfo`)
       .its('body')
       .then(({ aud, sub } = {}) => {
-        expect(sub).to.eq('foo@bar.com');
-        expect(aud).to.not.be.empty;
-      });
-  });
-});
+        expect(sub).to.eq('foo@bar.com')
+        expect(aud).to.not.be.empty
+      })
+  })
+})

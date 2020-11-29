37c9c71a9b4ae987c077f0fd7e52c236ee54cf4c
aeneasr <aeneasr@users.noreply.github.com>
2020-11-29 09:11:40 +0000

    chore: update docusaurus template
    Signed-off-by: aeneasr <aeneasr@users.noreply.github.com>

--- docs/src/theme/Footer/index.js
+++ docs/src/theme/Footer/index.js
@@ -4,18 +4,18 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-import React from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import {useThemeConfig} from '@docusaurus/theme-common';
-import useBaseUrl from '@docusaurus/useBaseUrl';
-import styles from './styles.module.css';
+import React from 'react'
+import clsx from 'clsx'
+import Link from '@docusaurus/Link'
+import { useThemeConfig } from '@docusaurus/theme-common'
+import useBaseUrl from '@docusaurus/useBaseUrl'
+import styles from './styles.module.css'
 
-function FooterLink({to, href, label, prependBaseUrlToHref, ...props}) {
-  const toUrl = useBaseUrl(to);
+function FooterLink({ to, href, label, prependBaseUrlToHref, ...props }) {
+  const toUrl = useBaseUrl(to)
   const normalizedHref = useBaseUrl(href, {
-    forcePrependBaseUrl: true,
-  });
+    forcePrependBaseUrl: true
+  })
   return (
     <Link
       className="footer__link-item"
@@ -23,35 +23,37 @@ function FooterLink({to, href, label, prependBaseUrlToHref, ...props}) {
         ? {
             target: '_blank',
             rel: 'noopener noreferrer',
-            href: prependBaseUrlToHref ? normalizedHref : href,
+            href: prependBaseUrlToHref ? normalizedHref : href
           }
         : {
-            to: toUrl,
+            to: toUrl
           })}
-      {...props}>
+      {...props}
+    >
       {label}
     </Link>
-  );
+  )
 }
 
-const FooterLogo = ({url, alt}) => (
+const FooterLogo = ({ url, alt }) => (
   <img className="footer__logo" alt={alt} src={url} />
-);
+)
 
 function Footer() {
-  const {footer} = useThemeConfig();
-  const {copyright, links = [], logo = {}} = footer || {};
-  const logoUrl = useBaseUrl(logo.src);
+  const { footer } = useThemeConfig()
+  const { copyright, links = [], logo = {} } = footer || {}
+  const logoUrl = useBaseUrl(logo.src)
 
   if (!footer) {
-    return null;
+    return null
   }
 
   return (
     <footer
       className={clsx('footer', {
-        'footer--dark': footer.style === 'dark',
-      })}>
+        'footer--dark': footer.style === 'dark'
+      })}
+    >
       <div className="container">
         {links && links.length > 0 && (
           <div className="row footer__links">
@@ -71,14 +73,14 @@ function Footer() {
                           className="footer__item" // Developer provided the HTML, so assume it's safe.
                           // eslint-disable-next-line react/no-danger
                           dangerouslySetInnerHTML={{
-                            __html: item.html,
+                            __html: item.html
                           }}
                         />
                       ) : (
                         <li key={item.href || item.to} className="footer__item">
                           <FooterLink {...item} />
                         </li>
-                      ),
+                      )
                     )}
                   </ul>
                 ) : null}
@@ -95,7 +97,8 @@ function Footer() {
                     href={logo.href}
                     target="_blank"
                     rel="noopener noreferrer"
-                    className={styles.footerLogoLink}>
+                    className={styles.footerLogoLink}
+                  >
                     <FooterLogo alt={logo.alt} url={logoUrl} />
                   </a>
                 ) : (
@@ -107,14 +110,14 @@ function Footer() {
             <div // Developer provided the HTML, so assume it's safe.
               // eslint-disable-next-line react/no-danger
               dangerouslySetInnerHTML={{
-                __html: copyright,
+                __html: copyright
               }}
             />
           </div>
         )}
       </div>
     </footer>
-  );
+  )
 }
 
-export default Footer;
+export default Footer

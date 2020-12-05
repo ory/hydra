93c6b2275efd37527fa83e1898a186e4b2775d74
aeneasr <aeneasr@users.noreply.github.com>
2020-12-05 10:54:01 +0000

    chore: update docusaurus template
    Signed-off-by: aeneasr <aeneasr@users.noreply.github.com>

--- docs/src/plugins/docusaurus-plugin-matamo/index.js
+++ docs/src/plugins/docusaurus-plugin-matamo/index.js
@@ -36,7 +36,7 @@ _paq.push(['enableLinkTracking']);
   _paq.push(['setTrackerUrl', u+'np.php']);
   _paq.push(['setSiteId', '1']);
   var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-  g.type='text/javascript'; g.async=true; g.src=u+'np.js'; s.parentNode.insertBefore(g,s);
+  g.type='text/javascript'; g.async=true; g.src=u+'js/np.js'; s.parentNode.insertBefore(g,s);
 })();
 `
           }

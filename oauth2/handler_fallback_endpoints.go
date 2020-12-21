f9b820521b7aad121e2c746f276711e54ffdb910
hackerman <3372410+aeneasr@users.noreply.github.com>
2020-12-21 10:02:23 +0100

    docs: OAUTH2_ERROR_URL -> URLS_ERROR (#2263)

--- oauth2/handler_fallback_endpoints.go
+++ oauth2/handler_fallback_endpoints.go
@@ -76,7 +76,7 @@ func (h *Handler) fallbackHandler(title, heading string, sc int, configKey strin
 }
 
 func (h *Handler) DefaultErrorHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	h.r.Logger().WithRequest(r).Error("A client requested the default error URL, environment variable OAUTH2_ERROR_URL is probably not set.")
+	h.r.Logger().WithRequest(r).Error("A client requested the default error URL, environment variable URLS_ERROR is probably not set.")
 
 	t, err := template.New("consent").Parse(`
 <html>

866c25a1b274635bf02e213b68706db62e2e46ad
aeneasr <aeneas@ory.sh>
2021-01-12 12:19:06 +0000

    autogen(openapi): Regenerate swagger spec and internal client

--- internal/httpclient/client/admin/delete_json_web_key_responses.go
+++ internal/httpclient/client/admin/delete_json_web_key_responses.go
@@ -49,7 +49,7 @@ func (o *DeleteJSONWebKeyReader) ReadResponse(response runtime.ClientResponse, c
 		return nil, result
 
 	default:
-		return nil, runtime.NewAPIError("unknown error", response, response.Code())
+		return nil, runtime.NewAPIError("response status code does not match any response statuses defined for this endpoint in the swagger spec", response, response.Code())
 	}
 }
 
--- internal/httpclient/client/admin/reject_logout_request_responses.go
+++ internal/httpclient/client/admin/reject_logout_request_responses.go
@@ -43,7 +43,7 @@ func (o *RejectLogoutRequestReader) ReadResponse(response runtime.ClientResponse
 		return nil, result
 
 	default:
-		return nil, runtime.NewAPIError("unknown error", response, response.Code())
+		return nil, runtime.NewAPIError("response status code does not match any response statuses defined for this endpoint in the swagger spec", response, response.Code())
 	}
 }
 

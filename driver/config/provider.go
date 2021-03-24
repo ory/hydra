dc6354f1e82ce5214dd493b2b617af3ac699f946
Vladimir Kalugin <v.p.kalugin@tinkoff.ru>
2021-03-24 15:08:43 +0300

    feat(config): changed config names for grant jwt

--- driver/config/provider.go
+++ driver/config/provider.go
@@ -65,9 +65,9 @@ const (
 	KeyOAuth2LegacyErrors                        = "oauth2.include_legacy_error_fields"
 	KeyExcludeNotBeforeClaim                     = "oauth2.exclude_not_before_claim"
 	KeyOAuth2GrantJWTClientAuthOptional          = "oauth2.grant.jwt.client_auth_optional"
-	KeyOAuth2GrantJWTIDOptional                  = "oauth2.grant.jwt.id_optional"
-	KeyOAuth2GrantJWTIssuedDateOptional          = "oauth2.grant.jwt.issued_date_optional"
-	KeyOAuth2GrantJWTMaxDuration                 = "oauth2.grant.jwt.max_duration"
+	KeyOAuth2GrantJWTIDOptional                  = "oauth2.grant.jwt.jti_optional"
+	KeyOAuth2GrantJWTIssuedDateOptional          = "oauth2.grant.jwt.iat_optional"
+	KeyOAuth2GrantJWTMaxDuration                 = "oauth2.grant.jwt.max_ttl"
 )
 
 const DSNMemory = "memory"

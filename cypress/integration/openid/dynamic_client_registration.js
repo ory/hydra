e5b9dedef88261953033f3c2ca1113d19c451576
fjviera <javier.viera@mindcurv.com>
2021-06-15 09:45:46 +0200

    Add dynamic client registration

--- cypress/integration/openid/dynamic_client_registration.js
+++ cypress/integration/openid/dynamic_client_registration.js
@@ -1,9 +1,7 @@
 import { prng } from '../../helpers'
 
 describe('The Clients Pubic Interface', function () {
-
   it('should return same client_secret given in request for newly created clients with client_secret specified', function () {
-
     cy.request({
       method: 'POST',
       url: Cypress.env('public_url') + '/clients',
@@ -12,9 +10,7 @@ describe('The Clients Pubic Interface', function () {
         client_name: 'clientName',
         client_secret: 'secret',
         scope: 'foo openid offline_access',
-        grant_types: [
-          'client_credentials'
-        ]
+        grant_types: ['client_credentials']
       }
     }).then((response) => {
       console.log(response.body)
@@ -23,7 +19,6 @@ describe('The Clients Pubic Interface', function () {
   })
 
   it('should get client when having a valid client_secret in body', function () {
-
     cy.request({
       method: 'GET',
       url: Cypress.env('public_url') + '/clients/clientid?secret=secret'
@@ -34,18 +29,16 @@ describe('The Clients Pubic Interface', function () {
   })
 
   it('should fail for get client when having an invalid client_secret in body', function () {
-
     cy.request({
       method: 'GET',
       failOnStatusCode: false,
       url: Cypress.env('public_url') + '/clients/clientid?secret=wrongsecret'
     }).then((response) => {
-      expect(response.status).to.eq(401);
+      expect(response.status).to.eq(401)
     })
   })
 
   it('should update client name when having a valid client_secret in body', function () {
-
     cy.request({
       method: 'PUT',
       url: Cypress.env('public_url') + '/clients/clientid',
@@ -54,9 +47,7 @@ describe('The Clients Pubic Interface', function () {
         client_name: 'clientName2',
         client_secret: 'secret',
         scope: 'foo openid offline_access',
-        grant_types: [
-          'client_credentials'
-        ]
+        grant_types: ['client_credentials']
       }
     }).then((response) => {
       console.log(response.body)
@@ -65,7 +56,6 @@ describe('The Clients Pubic Interface', function () {
   })
 
   it('should fail to update client name when having an invalid client_secret in body', function () {
-
     cy.request({
       method: 'PUT',
       failOnStatusCode: false,
@@ -75,36 +65,30 @@ describe('The Clients Pubic Interface', function () {
         client_name: 'clientName2',
         client_secret: 'wrongsecret',
         scope: 'foo openid offline_access',
-        grant_types: [
-          'client_credentials'
-        ]
+        grant_types: ['client_credentials']
       }
     }).then((response) => {
-      expect(response.status).to.eq(401);
+      expect(response.status).to.eq(401)
     })
   })
 
   it('should fail to delete client when having an invalid client_secret as parameter body', function () {
-
     cy.request({
       method: 'DELETE',
       failOnStatusCode: false,
       url: Cypress.env('public_url') + '/clients/clientid?secret=wrongsecret'
     }).then((response) => {
-      expect(response.status).to.eq(401);
+      expect(response.status).to.eq(401)
     })
   })
 
   it('should delete client when having an valid client_secret as parameter body', function () {
-
     cy.request({
       method: 'DELETE',
       failOnStatusCode: false,
       url: Cypress.env('public_url') + '/clients/clientid?secret=secret'
     }).then((response) => {
-      expect(response.status).to.eq(204);
+      expect(response.status).to.eq(204)
     })
   })
-
-
 })

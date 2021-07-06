9cfec9d1a9970a9515d463b00f3d20af0a9d8ddd
hackerman <3372410+aeneasr@users.noreply.github.com>
2021-07-06 11:10:05 +0200

    chore: update docusaurus template (#2619)
    Signed-off-by: aeneasr <aeneasr@users.noreply.github.com>
    
    Co-authored-by: aeneasr <aeneasr@users.noreply.github.com>

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -11,30 +11,7 @@ let links = [
     to: 'https://www.ory.sh/',
     label: `Home`,
     position: 'left'
-  }
-]
-
-if (config.enableRedoc) {
-  links = [
-    ...links,
-    {
-      to: baseUrl,
-      label: `Docs`,
-      position: 'left',
-      activeBaseRegex: '/docs/(?!http-api).*'
-    },
-    {
-      type: 'docsVersion',
-      position: 'left',
-      to: '/http-api',
-      label: 'HTTP API',
-      docsPluginId: 'default'
-    }
-  ]
-}
-
-links = [
-  ...links,
+  },
   {
     href: `https://github.com/ory/${githubRepoName}/discussions`,
     label: 'Discussions',
@@ -220,6 +197,7 @@ module.exports = {
         customCss
       }
     ],
-    '@docusaurus/theme-search-algolia'
+    '@docusaurus/theme-search-algolia',
+    'docusaurus-theme-redoc'
   ]
 }

02a9a7e810f0f26fe45bfe07ea09e92acfaa8cc0
aeneasr <3372410+aeneasr@users.noreply.github.com>
2021-07-13 15:58:17 +0200

    feat: code review

--- cypress/integration/admin/grant_jwtbearer.js
+++ cypress/integration/admin/grant_jwtbearer.js
@@ -1,5 +1,11 @@
+const dayjs = require('dayjs')
+const isBetween = require('dayjs/plugin/isBetween')
+const utc = require('dayjs/plugin/utc')
+dayjs.extend(utc)
+dayjs.extend(isBetween)
+
 describe('The JWT-Bearer Grants Admin Interface', () => {
-  let d = Cypress.moment().add(1, 'year').milliseconds(0).utc()
+  let d = dayjs().utc().add(1, 'year').set('millisecond', 0)
   const newGrant = (issuer = 'token-service', subject = 'bob@example.com') => ({
     issuer,
     subject,
@@ -17,25 +23,31 @@ describe('The JWT-Bearer Grants Admin Interface', () => {
   })
 
   beforeEach(() => {
-    // Delete all grants
-    cy.request('DELETE', Cypress.env('admin_url') + '/grants/jwt-bearer').then(
-      (response) => {
-        expect(response.body).to.length(1)
-      }
-    )
+    // Clean up all previous grants
+    cy.request(
+      'GET',
+      Cypress.env('admin_url') + '/trust/grants/jwt-bearer/issuers'
+    ).then((response) => {
+      response.body.map(({ id }) => {
+        cy.request(
+          'delete',
+          Cypress.env('admin_url') + '/trust/grants/jwt-bearer/issuers/' + id
+        ).then(() => {})
+      })
+    })
   })
 
   it('should return newly created jwt-bearer grant and grant can be retrieved later', () => {
     const grant = newGrant()
-    const start = Cypress.moment().subtract(1, 'minutes').utc()
-    const end = Cypress.moment().add(1, 'minutes').utc()
+    const start = dayjs().subtract(1, 'minutes')
+    const end = dayjs().add(1, 'minutes')
     cy.request(
       'POST',
-      Cypress.env('admin_url') + '/grants/jwt-bearer',
+      Cypress.env('admin_url') + '/trust/grants/jwt-bearer/issuers',
       JSON.stringify(grant)
     ).then((response) => {
-      const createdAt = Cypress.moment(response.body.created_at)
-      const expiresAt = Cypress.moment(response.body.expires_at)
+      const createdAt = dayjs(response.body.created_at)
+      const expiresAt = dayjs(response.body.expires_at)
       const grantID = response.body.id
 
       expect(response.body.issuer).to.equal(grant.issuer)
@@ -48,7 +60,7 @@ describe('The JWT-Bearer Grants Admin Interface', () => {
 
       cy.request(
         'GET',
-        Cypress.env('admin_url') + '/grants/jwt-bearer/' + grantID
+        Cypress.env('admin_url') + '/trust/grants/jwt-bearer/issuers/' + grantID
       ).then((response) => {
         expect(response.body.issuer).to.equal(grant.issuer)
         expect(response.body.subject).to.equal(grant.subject)
@@ -61,18 +73,34 @@ describe('The JWT-Bearer Grants Admin Interface', () => {
 
   it('should return newly created jwt-bearer grant in grants list', () => {
     // We have exactly one grant
-    cy.request('GET', Cypress.env('admin_url') + '/grants/jwt-bearer').then(
-      (response) => {
-        expect(response.body).to.length(1)
-      }
-    )
+    const grant = newGrant()
+    cy.request(
+      'POST',
+      Cypress.env('admin_url') + '/trust/grants/jwt-bearer/issuers',
+      JSON.stringify(grant)
+    ).then(() => {})
+    cy.request(
+      'GET',
+      Cypress.env('admin_url') + '/trust/grants/jwt-bearer/issuers'
+    ).then((response) => {
+      expect(response.body).to.length(1)
+    })
   })
 
   it('should fail, because the same grant is already exist', () => {
     const grant = newGrant()
     cy.request({
       method: 'POST',
-      url: Cypress.env('admin_url') + '/grants/jwt-bearer',
+      url: Cypress.env('admin_url') + '/trust/grants/jwt-bearer/issuers',
+      failOnStatusCode: false,
+      body: JSON.stringify(grant)
+    }).then((response) => {
+      expect(response.status).to.equal(201)
+    })
+
+    cy.request({
+      method: 'POST',
+      url: Cypress.env('admin_url') + '/trust/grants/jwt-bearer/issuers',
       failOnStatusCode: false,
       body: JSON.stringify(grant)
     }).then((response) => {
@@ -85,7 +113,7 @@ describe('The JWT-Bearer Grants Admin Interface', () => {
     grant.issuer = ''
     cy.request({
       method: 'POST',
-      url: Cypress.env('admin_url') + '/grants/jwt-bearer',
+      url: Cypress.env('admin_url') + '/trust/grants/jwt-bearer/issuers',
       failOnStatusCode: false,
       body: JSON.stringify(grant)
     }).then((response) => {

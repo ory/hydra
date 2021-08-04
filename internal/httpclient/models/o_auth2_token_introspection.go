cbf1c976fa8259a3d2c4cd9f6d3b54d4d1383179
aeneasr <3372410+aeneasr@users.noreply.github.com>
2021-08-04 14:51:04 +0000

    autogen(openapi): Regenerate swagger spec and internal client

--- internal/httpclient/models/o_auth2_token_introspection.go
+++ internal/httpclient/models/o_auth2_token_introspection.go
@@ -6,6 +6,8 @@ package models
 // Editing this file might prove futile when you re-run the swagger generate command
 
 import (
+	"context"
+
 	"github.com/go-openapi/errors"
 	"github.com/go-openapi/strfmt"
 	"github.com/go-openapi/swag"
@@ -102,6 +104,11 @@ func (m *OAuth2TokenIntrospection) validateActive(formats strfmt.Registry) error
 		return err
 	}
 
+	return nil
+}
+
+// ContextValidate validates this o auth2 token introspection based on context it is used
+func (m *OAuth2TokenIntrospection) ContextValidate(ctx context.Context, formats strfmt.Registry) error {
 	return nil
 }
 

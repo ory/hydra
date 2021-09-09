ab50e02d5bc24df651a39b41500fb6c76d235072
aeneasr <aeneasr@users.noreply.github.com>
2021-09-09 07:39:33 +0000

    chore: update docusaurus template
    Signed-off-by: aeneasr <aeneasr@users.noreply.github.com>

--- docs/scripts/config.js
+++ docs/scripts/config.js
@@ -220,9 +220,6 @@ title: Configuration
 OPEN AN ISSUE IF YOU WOULD LIKE TO MAKE ADJUSTMENTS HERE AND MAINTAINERS WILL HELP YOU LOCATE THE RIGHT
 FILE -->
 
-If file \`$HOME/.${config.projectSlug}.yaml\` exists, it will be used as a configuration file which supports all
-configuration settings listed below.
-
 You can load the config file from another source using the \`-c path/to/config.yaml\` or \`--config path/to/config.yaml\`
 flag: \`${config.projectSlug} --config path/to/config.yaml\`.
 

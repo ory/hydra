b12e70c9a08ad62490731f57b5bfcb52c64217f0
hackerman <3372410+aeneasr@users.noreply.github.com>
2021-09-21 07:17:18 +0200

    feat: add method to detect public keys without prefixing (#2758)
    Closes #2459

--- internal/httpclient/client/admin/create_o_auth2_client_parameters.go
+++ internal/httpclient/client/admin/create_o_auth2_client_parameters.go
@@ -18,56 +18,70 @@ import (
 	"github.com/ory/hydra/internal/httpclient/models"
 )
 
-// NewCreateOAuth2ClientParams creates a new CreateOAuth2ClientParams object
-// with the default values initialized.
+// NewCreateOAuth2ClientParams creates a new CreateOAuth2ClientParams object,
+// with the default timeout for this client.
+//
+// Default values are not hydrated, since defaults are normally applied by the API server side.
+//
+// To enforce default values in parameter, use SetDefaults or WithDefaults.
 func NewCreateOAuth2ClientParams() *CreateOAuth2ClientParams {
-	var ()
 	return &CreateOAuth2ClientParams{
-
 		timeout: cr.DefaultTimeout,
 	}
 }
 
 // NewCreateOAuth2ClientParamsWithTimeout creates a new CreateOAuth2ClientParams object
-// with the default values initialized, and the ability to set a timeout on a request
+// with the ability to set a timeout on a request.
 func NewCreateOAuth2ClientParamsWithTimeout(timeout time.Duration) *CreateOAuth2ClientParams {
-	var ()
 	return &CreateOAuth2ClientParams{
-
 		timeout: timeout,
 	}
 }
 
 // NewCreateOAuth2ClientParamsWithContext creates a new CreateOAuth2ClientParams object
-// with the default values initialized, and the ability to set a context for a request
+// with the ability to set a context for a request.
 func NewCreateOAuth2ClientParamsWithContext(ctx context.Context) *CreateOAuth2ClientParams {
-	var ()
 	return &CreateOAuth2ClientParams{
-
 		Context: ctx,
 	}
 }
 
 // NewCreateOAuth2ClientParamsWithHTTPClient creates a new CreateOAuth2ClientParams object
-// with the default values initialized, and the ability to set a custom HTTPClient for a request
+// with the ability to set a custom HTTPClient for a request.
 func NewCreateOAuth2ClientParamsWithHTTPClient(client *http.Client) *CreateOAuth2ClientParams {
-	var ()
 	return &CreateOAuth2ClientParams{
 		HTTPClient: client,
 	}
 }
 
-/*CreateOAuth2ClientParams contains all the parameters to send to the API endpoint
-for the create o auth2 client operation typically these are written to a http.Request
+/* CreateOAuth2ClientParams contains all the parameters to send to the API endpoint
+   for the create o auth2 client operation.
+
+   Typically these are written to a http.Request.
 */
 type CreateOAuth2ClientParams struct {
 
-	/*Body*/
+	// Body.
 	Body *models.OAuth2Client
 
 	timeout    time.Duration
 	Context    context.Context
 	HTTPClient *http.Client
+}
+
+// WithDefaults hydrates default values in the create o auth2 client params (not the query body).
+//
+// All values with no default are reset to their zero value.
+func (o *CreateOAuth2ClientParams) WithDefaults() *CreateOAuth2ClientParams {
+	o.SetDefaults()
+	return o
+}
+
+// SetDefaults hydrates default values in the create o auth2 client params (not the query body).
+//
+// All values with no default are reset to their zero value.
+func (o *CreateOAuth2ClientParams) SetDefaults() {
+	// no default values defined for this parameter
 }
 
 // WithTimeout adds the timeout to the create o auth2 client params
@@ -121,7 +135,6 @@ func (o *CreateOAuth2ClientParams) WriteToRequest(r runtime.ClientRequest, reg s
 		return err
 	}
 	var res []error
-
 	if o.Body != nil {
 		if err := r.SetBodyParam(o.Body); err != nil {
 			return err
--- internal/httpclient/client/admin/is_instance_alive_parameters.go
+++ internal/httpclient/client/admin/is_instance_alive_parameters.go
@@ -16,52 +16,66 @@ import (
 	"github.com/go-openapi/strfmt"
 )
 
-// NewIsInstanceAliveParams creates a new IsInstanceAliveParams object
-// with the default values initialized.
+// NewIsInstanceAliveParams creates a new IsInstanceAliveParams object,
+// with the default timeout for this client.
+//
+// Default values are not hydrated, since defaults are normally applied by the API server side.
+//
+// To enforce default values in parameter, use SetDefaults or WithDefaults.
 func NewIsInstanceAliveParams() *IsInstanceAliveParams {
-
 	return &IsInstanceAliveParams{
-
 		timeout: cr.DefaultTimeout,
 	}
 }
 
 // NewIsInstanceAliveParamsWithTimeout creates a new IsInstanceAliveParams object
-// with the default values initialized, and the ability to set a timeout on a request
+// with the ability to set a timeout on a request.
 func NewIsInstanceAliveParamsWithTimeout(timeout time.Duration) *IsInstanceAliveParams {
-
 	return &IsInstanceAliveParams{
-
 		timeout: timeout,
 	}
 }
 
 // NewIsInstanceAliveParamsWithContext creates a new IsInstanceAliveParams object
-// with the default values initialized, and the ability to set a context for a request
+// with the ability to set a context for a request.
 func NewIsInstanceAliveParamsWithContext(ctx context.Context) *IsInstanceAliveParams {
-
 	return &IsInstanceAliveParams{
-
 		Context: ctx,
 	}
 }
 
 // NewIsInstanceAliveParamsWithHTTPClient creates a new IsInstanceAliveParams object
-// with the default values initialized, and the ability to set a custom HTTPClient for a request
+// with the ability to set a custom HTTPClient for a request.
 func NewIsInstanceAliveParamsWithHTTPClient(client *http.Client) *IsInstanceAliveParams {
-
 	return &IsInstanceAliveParams{
 		HTTPClient: client,
 	}
 }
 
-/*IsInstanceAliveParams contains all the parameters to send to the API endpoint
-for the is instance alive operation typically these are written to a http.Request
+/* IsInstanceAliveParams contains all the parameters to send to the API endpoint
+   for the is instance alive operation.
+
+   Typically these are written to a http.Request.
 */
 type IsInstanceAliveParams struct {
 	timeout    time.Duration
 	Context    context.Context
 	HTTPClient *http.Client
+}
+
+// WithDefaults hydrates default values in the is instance alive params (not the query body).
+//
+// All values with no default are reset to their zero value.
+func (o *IsInstanceAliveParams) WithDefaults() *IsInstanceAliveParams {
+	o.SetDefaults()
+	return o
+}
+
+// SetDefaults hydrates default values in the is instance alive params (not the query body).
+//
+// All values with no default are reset to their zero value.
+func (o *IsInstanceAliveParams) SetDefaults() {
+	// no default values defined for this parameter
 }
 
 // WithTimeout adds the timeout to the is instance alive params

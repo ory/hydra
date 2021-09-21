3baa4b790cecb89edfae5103e2a7bfcf21f97f01
aeneasr <3372410+aeneasr@users.noreply.github.com>
2021-09-21 06:51:08 +0000

    autogen(docs): generate and format documentation

--- internal/httpclient/models/plugin_config_user.go
+++ internal/httpclient/models/plugin_config_user.go
@@ -6,8 +6,6 @@ package models
 // Editing this file might prove futile when you re-run the swagger generate command
 
 import (
-	"context"
-
 	"github.com/go-openapi/strfmt"
 	"github.com/go-openapi/swag"
 )
@@ -26,11 +24,6 @@ type PluginConfigUser struct {
 
 // Validate validates this plugin config user
 func (m *PluginConfigUser) Validate(formats strfmt.Registry) error {
-	return nil
-}
-
-// ContextValidate validates this plugin config user based on context it is used
-func (m *PluginConfigUser) ContextValidate(ctx context.Context, formats strfmt.Registry) error {
 	return nil
 }
 

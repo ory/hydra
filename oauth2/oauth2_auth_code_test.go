48841757f3161c733a7c7b08f710e321bc24f135
Felix Jung <felix.jung@sumup.com>
2021-09-29 16:37:50 +0200

    Add AMR claim test

--- oauth2/oauth2_auth_code_test.go
+++ oauth2/oauth2_auth_code_test.go
@@ -145,6 +145,7 @@ func TestAuthCodeWithDefaultStrategy(t *testing.T) {
 				Subject:  &subject,
 				Remember: !*rr.Payload.Skip,
 				Acr:      "1",
+				Amr:      models.StringSlicePipeDelimiter{"pwd"},
 				Context:  map[string]interface{}{"context": "bar"},
 			}
 			if checkRequestPayload != nil {
@@ -213,6 +214,8 @@ func TestAuthCodeWithDefaultStrategy(t *testing.T) {
 		assert.EqualValues(t, reg.Config().IssuerURL().String(), claims.Get("iss").String(), "%s", claims)
 		assert.NotEmpty(t, claims.Get("sid").String(), "%s", claims)
 		assert.Equal(t, "1", claims.Get("acr").String(), "%s", claims)
+		require.Len(t, claims.Get("amr").Array(), 1, "%s", claims)
+		assert.EqualValues(t, "pwd", claims.Get("amr").Array()[0].String(), "%s", claims)
 
 		require.Len(t, claims.Get("aud").Array(), 1, "%s", claims)
 		assert.EqualValues(t, c.ClientID, claims.Get("aud").Array()[0].String(), "%s", claims)

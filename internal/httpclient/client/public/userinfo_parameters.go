f634b4832b61200f280e6df951db356b222a30d6
aeneasr <3372410+aeneasr@users.noreply.github.com>
2021-09-29 11:07:15 +0000

    autogen(openapi): Regenerate swagger spec and internal client

--- internal/httpclient/client/public/userinfo_parameters.go
+++ internal/httpclient/client/public/userinfo_parameters.go
@@ -16,52 +16,66 @@ import (
 	"github.com/go-openapi/strfmt"
 )
 
-// NewUserinfoParams creates a new UserinfoParams object
-// with the default values initialized.
+// NewUserinfoParams creates a new UserinfoParams object,
+// with the default timeout for this client.
+//
+// Default values are not hydrated, since defaults are normally applied by the API server side.
+//
+// To enforce default values in parameter, use SetDefaults or WithDefaults.
 func NewUserinfoParams() *UserinfoParams {
-
 	return &UserinfoParams{
-
 		timeout: cr.DefaultTimeout,
 	}
 }
 
 // NewUserinfoParamsWithTimeout creates a new UserinfoParams object
-// with the default values initialized, and the ability to set a timeout on a request
+// with the ability to set a timeout on a request.
 func NewUserinfoParamsWithTimeout(timeout time.Duration) *UserinfoParams {
-
 	return &UserinfoParams{
-
 		timeout: timeout,
 	}
 }
 
 // NewUserinfoParamsWithContext creates a new UserinfoParams object
-// with the default values initialized, and the ability to set a context for a request
+// with the ability to set a context for a request.
 func NewUserinfoParamsWithContext(ctx context.Context) *UserinfoParams {
-
 	return &UserinfoParams{
-
 		Context: ctx,
 	}
 }
 
 // NewUserinfoParamsWithHTTPClient creates a new UserinfoParams object
-// with the default values initialized, and the ability to set a custom HTTPClient for a request
+// with the ability to set a custom HTTPClient for a request.
 func NewUserinfoParamsWithHTTPClient(client *http.Client) *UserinfoParams {
-
 	return &UserinfoParams{
 		HTTPClient: client,
 	}
 }
 
-/*UserinfoParams contains all the parameters to send to the API endpoint
-for the userinfo operation typically these are written to a http.Request
+/* UserinfoParams contains all the parameters to send to the API endpoint
+   for the userinfo operation.
+
+   Typically these are written to a http.Request.
 */
 type UserinfoParams struct {
 	timeout    time.Duration
 	Context    context.Context
 	HTTPClient *http.Client
+}
+
+// WithDefaults hydrates default values in the userinfo params (not the query body).
+//
+// All values with no default are reset to their zero value.
+func (o *UserinfoParams) WithDefaults() *UserinfoParams {
+	o.SetDefaults()
+	return o
+}
+
+// SetDefaults hydrates default values in the userinfo params (not the query body).
+//
+// All values with no default are reset to their zero value.
+func (o *UserinfoParams) SetDefaults() {
+	// no default values defined for this parameter
 }
 
 // WithTimeout adds the timeout to the userinfo params

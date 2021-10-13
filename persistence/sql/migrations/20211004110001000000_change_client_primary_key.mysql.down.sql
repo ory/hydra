603050d8eb632227e3bfa26100ff1162a548b073
Grant Zvolský <GrantZvolsky@gmail.com>
2021-10-13 09:55:51 +0200

    fix: fix hydra_client pk change mysql down migration (#2791)

--- persistence/sql/migrations/20211004110001000000_change_client_primary_key.mysql.down.sql
+++ persistence/sql/migrations/20211004110001000000_change_client_primary_key.mysql.down.sql
@@ -1,4 +1,4 @@
-ALTER TABLE hydra_client RENAME pk TO pk_tmp;
+ALTER TABLE hydra_client DROP PRIMARY KEY, ADD PRIMARY KEY (pk_deprecated);
+ALTER TABLE hydra_client DROP KEY pk_deprecated;
+ALTER TABLE hydra_client DROP pk;
 ALTER TABLE hydra_client CHANGE COLUMN pk_deprecated pk INT UNSIGNED AUTO_INCREMENT;
-ALTER TABLE hydra_client DROP PRIMARY KEY, ADD PRIMARY KEY (pk);
-ALTER TABLE hydra_client DROP pk_tmp;

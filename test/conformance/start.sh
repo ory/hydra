a27e2821e7f6bb5471305dd6e38850e690cc7f19
hirasawayuki <hirasawa.yuki@zaim.co.jp>
2021-11-03 19:08:58 +0900

    fix: test/conformance/start.sh ignore SC2086

--- test/conformance/start.sh
+++ test/conformance/start.sh
@@ -3,4 +3,5 @@
 set -euxo pipefail
 cd "$( dirname "${BASH_SOURCE[0]}" )/../.."
 
-docker-compose -f quickstart.yml -f quickstart-postgres.yml -f test/conformance/docker-compose.yml up "${1:-}" -d
+# shellcheck disable=SC2086
+docker-compose -f quickstart.yml -f quickstart-postgres.yml -f test/conformance/docker-compose.yml up ${1:-} -d

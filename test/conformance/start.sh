38cbcc02a0689fa28c1ccd892e7069d1b34516a6
Yuki Hirasawa <48427044+hirasawayuki@users.noreply.github.com>
2021-11-09 17:26:50 +0900

    feat: add shellcheck to circleci (#2835)
    Closes #2832

--- test/conformance/start.sh
+++ test/conformance/start.sh
@@ -3,4 +3,5 @@
 set -euxo pipefail
 cd "$( dirname "${BASH_SOURCE[0]}" )/../.."
 
+# shellcheck disable=SC2086
 docker-compose -f quickstart.yml -f quickstart-postgres.yml -f test/conformance/docker-compose.yml up ${1:-} -d

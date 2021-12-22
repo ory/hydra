54138282b37651537f714c57400055a74dedfe41
Grant Zvolsky <grant@zvolsky.org>
2021-12-22 23:09:58 +0100

    code review: move db-diff to scripts & document what it does

--- scripts/db-diff.sh
+++ scripts/db-diff.sh
@@ -4,6 +4,22 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
+# This script is used to generate and compare the Hydra DDL at different
+# versions. This is useful when reviewing changes and troubleshooting
+# migrations.
+#
+# Side effects:
+# - Creates a directory at ./output/sql and stores the generated SQL
+#
+# Arguments:
+# $1 - database type (e.g. postgres, mysql, sqlite)
+# $2 - commit hash or branch name of the earlier version
+# $3 - commit hash or branch name of the later version
+#
+# Example output: (the script prints the command instead of executing it)
+# 'git diff --no-index ./output/sql/f5864b39.sqlite.dump.sql ./output/sql/master.sqlite.dump.sql'
+#
+
 # Usage
 # ./hack/db-diff.sh sqlite master 649f56cc
 if [ "$#" -ne 3 ]; then
@@ -35,21 +51,21 @@ function hydra::util::parse-connection-url {
 
 function hydra::util::ensure-sqlite {
 	if ! sqlite3 --version > /dev/null 2>&1; then
-		echo 'Error: sqlite3 is not installed;' >&2
+		echo 'Error: sqlite3 is not installed' >&2
 		exit 1
 	fi
 }
 
 function hydra::util::ensure-pg_dump {
 	if ! pg_dump --version > /dev/null 2>&1; then
-		echo 'Error: pg_dump is not installed;' >&2
+		echo 'Error: pg_dump is not installed' >&2
 		exit 1
 	fi
 }
 
 function hydra::util::ensure-mysqldump {
 	if ! mysqldump --version > /dev/null 2>&1; then
-		echo 'Error: mysqldump is not installed;' >&2
+		echo 'Error: mysqldump is not installed' >&2
 		exit 1
 	fi
 }
@@ -89,7 +105,7 @@ function dump_mysql {
 }
 
 if ! git diff-index --quiet HEAD --; then
-	echo 'Error: working tree is dirty;' >&2
+	echo 'Error: working tree is dirty' >&2
 	exit 1
 fi
 
@@ -115,22 +131,26 @@ esac
 DIALECT=$1
 COMMIT_FROM=$2
 COMMIT_TO=$3
-T_FROM="./output/sql/$COMMIT_FROM.$DIALECT.dump.sql"
-T_TO="./output/sql/$COMMIT_TO.$DIALECT.dump.sql"
+DDL_FROM="./output/sql/$COMMIT_FROM.$DIALECT.dump.sql"
+DDL_TO="./output/sql/$COMMIT_TO.$DIALECT.dump.sql"
 
 mkdir -p ./output/sql/
 
 set -x
-git checkout $COMMIT_FROM >/dev/null 2>&1
-# set +x
-$DUMP_CMD > $T_FROM
+# shellcheck disable=SC2064
+trap "git checkout -q $(git symbolic-ref HEAD); git symbolic-ref HEAD $(git symbolic-ref HEAD)" EXIT
 
-git checkout $COMMIT_TO >/dev/null 2>&1
-$DUMP_CMD > $T_TO
+git checkout "$COMMIT_FROM" >/dev/null 2>&1
+$DUMP_CMD > "$DDL_FROM"
 
-git checkout @{-2} >/dev/null 2>&1
+git checkout "$COMMIT_TO" >/dev/null 2>&1
+$DUMP_CMD > "$DDL_TO"
+
 set +x
-
-echo '---'
-echo 'Use the following command to print the diff:'
-echo 'git diff --no-index '"$T_FROM"' '"$T_TO"
+echo '+--------------------------'
+echo '|'
+echo '| Use the following command to print the diff:'
+echo '| git diff --no-index '"$DDL_FROM"' '"$DDL_TO"
+echo '|'
+echo '+--------------------------'
+set -x

b7ecf2c1c3d73679c3775db74cdefd1ce9152dc5
aeneasr <aeneasr@users.noreply.github.com>
2022-01-06 09:56:45 +0000

    chore: update docusaurus template
    Signed-off-by: aeneasr <aeneasr@users.noreply.github.com>

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -188,6 +188,7 @@ module.exports = {
       }
     ],
     '@docusaurus/plugin-content-pages',
+    require.resolve('./src/plugins/ory-scripts-loader'),
     require.resolve('./src/plugins/docusaurus-plugin-matamo'),
     '@docusaurus/plugin-sitemap'
   ],

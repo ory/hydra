b75b20ab4b0ef77993e1c04f003fdd3a895a1364
aeneasr <aeneasr@users.noreply.github.com>
2022-01-10 07:34:15 +0000

    chore: update docusaurus template
    Signed-off-by: aeneasr <aeneasr@users.noreply.github.com>

--- docs/src/plugins/ory-scripts-loader/index.js
+++ docs/src/plugins/ory-scripts-loader/index.js
@@ -4,8 +4,13 @@ module.exports = function (context) {
   return {
     name: 'docusaurus-plugin-ory-web-script',
 
-    getClientModules() {
-      return [path.resolve(__dirname, './ory-scripts-loader')]
+    // https://docusaurus.io/docs/api/plugin-methods/lifecycle-apis#injectHtmlTags
+    injectHtmlTags({ content }) {
+      return {
+        postBodyTags: [
+          '<script src="https://www.ory.sh/scripts.js" async></script>'
+        ]
+      }
     }
   }
 }

140ff4fe14a3d374c788e64c4607b647ec2c2c71
Jagoba Gascón <jagoba@arima.eu>
2022-01-31 14:46:27 +0100

    fix: trusted jwt issuer limit mysql unique size

--- persistence/sql/migrations/20220127125900000000_domain_jwk_bearer.mysql.up.sql
+++ persistence/sql/migrations/20220127125900000000_domain_jwk_bearer.mysql.up.sql
@@ -4,4 +4,4 @@ ALTER TABLE hydra_oauth2_trusted_jwt_bearer_issuer
     DROP INDEX issuer;
 
 ALTER TABLE hydra_oauth2_trusted_jwt_bearer_issuer
-    ADD CONSTRAINT hydra_oauth2_trusted_jwt_bearer_issuer_issuer_subject_key_idx UNIQUE (issuer, subject, allowed_domain, key_id);
+    ADD CONSTRAINT hydra_oauth2_trusted_jwt_bearer_issuer_issuer_subject_key_idx UNIQUE (issuer(128), subject(128), allowed_domain(128), key_id);

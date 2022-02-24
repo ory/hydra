a500b16a45736844bb5bb79332103869a0f22497
Andreas Krause <a.krause@celonis.com>
2022-02-24 10:58:33 +0100

    fix bug that tokens issued for grant urn:ietf:params:oauth:grant-type:jwt-bearer miss required session parameters

--- oauth2/handler.go
+++ oauth2/handler.go
@@ -587,7 +587,7 @@ func (h *Handler) TokenHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if accessRequest.GetGrantTypes().ExactOne("client_credentials") {
+	if accessRequest.GetGrantTypes().ExactOne("client_credentials") || accessRequest.GetGrantTypes().ExactOne("urn:ietf:params:oauth:grant-type:jwt-bearer") {
 		var accessTokenKeyID string
 		if h.c.AccessTokenStrategy() == "jwt" {
 			accessTokenKeyID, err = h.r.AccessTokenJWTStrategy().GetPublicKeyID(r.Context())
@@ -598,7 +598,10 @@ func (h *Handler) TokenHandler(w http.ResponseWriter, r *http.Request) {
 			}
 		}
 
-		session.Subject = accessRequest.GetClient().GetID()
+		// only for client_credentials, otherwise Authentication is included in session
+		if accessRequest.GetGrantTypes().ExactOne("client_credentials") {
+			session.Subject = accessRequest.GetClient().GetID()
+		}
 		session.ClientID = accessRequest.GetClient().GetID()
 		session.KID = accessTokenKeyID
 		session.DefaultSession.Claims.Issuer = strings.TrimRight(h.c.IssuerURL().String(), "/") + "/"

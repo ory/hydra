0204e51ca538551ae554207bdc9191729242eff6
arnolf <1955506+arnolf@users.noreply.github.com>
2022-05-17 16:09:48 +0200

    fix: add missing sql down migration file to drop new indexes on requested_at columns

---
+++ persistence/sql/migrations/20220516123200000000_oauth2.down.sql
@@ -0,0 +1,4 @@
+DROP INDEX hydra_oauth2_access_expiry;
+DROP INDEX hydra_oauth2_refresh_expiry;
+DROP INDEX hydra_oauth2_authentication_request_expiry;
+DROP INDEX hydra_oauth2_consent_request_expiry;

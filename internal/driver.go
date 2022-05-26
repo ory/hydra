2c894cbc852a466cd955f1d6ca7d999890040c4c
aeneasr <3372410+aeneasr@users.noreply.github.com>
2022-05-26 13:50:37 +0200

    fix: dont close crdb for reuse purposes

--- internal/driver.go
+++ internal/driver.go
@@ -105,7 +105,6 @@ func ConnectToCRDB(t *testing.T) string {
 func NewEmptyTestCRDBServer(t *testing.T) string {
 	ts, err := testserver.NewTestServer()
 	require.NoError(t, err)
-	t.Cleanup(ts.Stop)
 
 	ts.PGURL().Scheme = "cockroach"
 	return ts.PGURL().String()

9d03440f940a7ca8594cc1c2a3408d7dcc51d3af
aeneasr <3372410+aeneasr@users.noreply.github.com>
2022-06-17 16:17:14 +0200

    test(e2e): fix jwt regression

--- cypress/integration/oauth2/jwt.js
+++ cypress/integration/oauth2/jwt.js
@@ -1,4 +1,4 @@
-import { prng } from '../../helpers'
+import { createClient, prng } from '../../helpers'
 
 describe('OAuth 2.0 JSON Web Token Access Tokens', () => {
   before(function () {
@@ -19,30 +19,32 @@ describe('OAuth 2.0 JSON Web Token Access Tokens', () => {
   })
 
   it('should return an Access Token in JWT format and validate it and a Refresh Token in opaque format', () => {
-    const client = nc()
-    cy.authCodeFlow(client, {
-      consent: { scope: ['offline_access'], createClient: true }
+    createClient(nc()).then((client) => {
+      cy.authCodeFlow(client, {
+        consent: { scope: ['offline_access'], createClient: true },
+        createClient: false
+      })
+
+      cy.request(`${Cypress.env('client_url')}/oauth2/refresh`)
+        .its('body')
+        .then((body) => {
+          const { result, token } = body
+          expect(result).to.equal('success')
+
+          expect(token.access_token).to.not.be.empty
+          expect(token.refresh_token).to.not.be.empty
+          expect(token.access_token.split('.').length).to.equal(3)
+          expect(token.refresh_token.split('.').length).to.equal(2)
+        })
+
+      cy.request(`${Cypress.env('client_url')}/oauth2/validate-jwt`)
+        .its('body')
+        .then((body) => {
+          console.log(body)
+          expect(body.sub).to.eq('foo@bar.com')
+          expect(body.client_id).to.eq(client.client_id)
+          expect(body.jti).to.not.be.empty
+        })
     })
-
-    cy.request(`${Cypress.env('client_url')}/oauth2/refresh`)
-      .its('body')
-      .then((body) => {
-        const { result, token } = body
-        expect(result).to.equal('success')
-
-        expect(token.access_token).to.not.be.empty
-        expect(token.refresh_token).to.not.be.empty
-        expect(token.access_token.split('.').length).to.equal(3)
-        expect(token.refresh_token.split('.').length).to.equal(2)
-      })
-
-    cy.request(`${Cypress.env('client_url')}/oauth2/validate-jwt`)
-      .its('body')
-      .then((body) => {
-        console.log(body)
-        expect(body.sub).to.eq('foo@bar.com')
-        expect(body.client_id).to.eq(client.client_id)
-        expect(body.jti).to.not.be.empty
-      })
   })
 })

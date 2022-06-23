75ee70eb6238b574755764d8df662a7755c5dfa9
aeneasr <3372410+aeneasr@users.noreply.github.com>
2022-06-23 11:39:26 +0200

    chore: update formatter and formatting

--- cypress/integration/oauth2/client_creds.js
+++ cypress/integration/oauth2/client_creds.js
@@ -1,28 +1,28 @@
-import { createClient, prng } from '../../helpers'
+import { createClient, prng } from "../../helpers"
 
-describe('The OAuth 2.0 Authorization Code Grant', function () {
+describe("The OAuth 2.0 Authorization Code Grant", function () {
   const nc = () => ({
     client_secret: prng(),
-    scope: 'foo openid offline_access',
-    grant_types: ['client_credentials']
+    scope: "foo openid offline_access",
+    grant_types: ["client_credentials"],
   })
 
-  it('should return an Access Token but not Refresh or ID Token for client_credentials flow', function () {
+  it("should return an Access Token but not Refresh or ID Token for client_credentials flow", function () {
     createClient(nc()).then((client) => {
       cy.request(
-        `${Cypress.env('client_url')}/oauth2/cc?client_id=${
+        `${Cypress.env("client_url")}/oauth2/cc?client_id=${
           client.client_id
         }&client_secret=${client.client_secret}&scope=${client.scope}`,
-        { failOnStatusCode: false }
+        { failOnStatusCode: false },
       )
-        .its('body')
+        .its("body")
         .then((body) => {
           const {
             result,
-            token: { access_token, id_token, refresh_token } = {}
+            token: { access_token, id_token, refresh_token } = {},
           } = body
 
-          expect(result).to.equal('success')
+          expect(result).to.equal("success")
           expect(access_token).to.not.be.empty
           expect(id_token).to.be.undefined
           expect(refresh_token).to.be.undefined
--- cypress/integration/oauth2/refresh_token.js
+++ cypress/integration/oauth2/refresh_token.js
@@ -1,62 +1,62 @@
-import { createClient, prng } from '../../helpers'
+import { createClient, prng } from "../../helpers"
 
-describe('The OAuth 2.0 Refresh Token Grant', function () {
+describe("The OAuth 2.0 Refresh Token Grant", function () {
   const nc = () => ({
     client_secret: prng(),
-    scope: 'offline_access openid',
-    redirect_uris: [`${Cypress.env('client_url')}/oauth2/callback`],
-    grant_types: ['authorization_code', 'refresh_token']
+    scope: "offline_access openid",
+    redirect_uris: [`${Cypress.env("client_url")}/oauth2/callback`],
+    grant_types: ["authorization_code", "refresh_token"],
   })
 
-  it('should return an Access and Refresh Token and refresh the Access Token', function () {
+  it("should return an Access and Refresh Token and refresh the Access Token", function () {
     const client = nc()
     cy.authCodeFlow(client, {
-      consent: { scope: ['offline_access'], createClient: true }
+      consent: { scope: ["offline_access"], createClient: true },
     })
 
-    cy.request(`${Cypress.env('client_url')}/oauth2/refresh`)
-      .its('body')
+    cy.request(`${Cypress.env("client_url")}/oauth2/refresh`)
+      .its("body")
       .then((body) => {
         const { result, token } = body
-        expect(result).to.equal('success')
+        expect(result).to.equal("success")
         expect(token.access_token).to.not.be.empty
         expect(token.refresh_token).to.not.be.empty
       })
   })
 
-  it('should return an Access, ID, and Refresh Token and refresh the Access Token and ID Token', function () {
+  it("should return an Access, ID, and Refresh Token and refresh the Access Token and ID Token", function () {
     const client = nc()
     cy.authCodeFlow(client, {
-      consent: { scope: ['offline_access', 'openid'], createClient: true }
+      consent: { scope: ["offline_access", "openid"], createClient: true },
     })
 
-    cy.request(`${Cypress.env('client_url')}/oauth2/refresh`)
-      .its('body')
+    cy.request(`${Cypress.env("client_url")}/oauth2/refresh`)
+      .its("body")
       .then((body) => {
         const { result, token } = body
-        expect(result).to.equal('success')
+        expect(result).to.equal("success")
         expect(token.access_token).to.not.be.empty
         expect(token.id_token).to.not.be.empty
         expect(token.refresh_token).to.not.be.empty
       })
   })
 
-  it('should revoke Refresh Token on reuse', function () {
-    const referrer = `${Cypress.env('client_url')}/empty`
+  it("should revoke Refresh Token on reuse", function () {
+    const referrer = `${Cypress.env("client_url")}/empty`
     cy.visit(referrer, {
-      failOnStatusCode: false
+      failOnStatusCode: false,
     })
 
     createClient({
-      scope: 'offline_access',
+      scope: "offline_access",
       redirect_uris: [referrer],
-      grant_types: ['authorization_code', 'refresh_token'],
-      response_types: ['code'],
-      token_endpoint_auth_method: 'none'
+      grant_types: ["authorization_code", "refresh_token"],
+      response_types: ["code"],
+      token_endpoint_auth_method: "none",
     }).then((client) => {
       cy.authCodeFlowBrowser(client, {
-        consent: { scope: ['offline_access'] },
-        createClient: false
+        consent: { scope: ["offline_access"] },
+        createClient: false,
       }).then((originalResponse) => {
         expect(originalResponse.status).to.eq(200)
         expect(originalResponse.body.refresh_token).to.not.be.empty
@@ -74,14 +74,14 @@ describe('The OAuth 2.0 Refresh Token Grant', function () {
               .refreshTokenBrowser(client, originalToken)
               .then((response) => {
                 expect(response.status).to.eq(401)
-                expect(response.body.error).to.eq('token_inactive')
+                expect(response.body.error).to.eq("token_inactive")
               })
               .then(() => cy.refreshTokenBrowser(client, refreshedToken))
               .then((response) => {
                 expect(response.status).to.eq(401)
-                expect(response.body.error).to.eq('token_inactive')
+                expect(response.body.error).to.eq("token_inactive")
               })
-          }
+          },
         )
       })
     })

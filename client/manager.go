6ce2e29d07c0fc04d9216bf64bf71f7d4469ab50
aeneasr <3372410+aeneasr@users.noreply.github.com>
2022-06-24 15:41:05 +0200

    refactor(client): replace limit and offset parameters with page_token and page_size
    BREAKING CHANGES: To better support TB-scale environments, the OAuth2 Client HTTP API's query parameters for pagination have changed from `limit` and `offset` to `page_token` and `page_size`. The `page_token` is an opaque string contained in the HTTP `Link` Header, which expresses the next, previous, first, and last page.

--- client/manager.go
+++ client/manager.go
@@ -26,6 +26,25 @@ import (
 	"github.com/ory/fosite"
 )
 
+// swagger:ignore
+type Filter struct {
+	// The maximum amount of clients to returned, upper bound is 500 clients.
+	// in: query
+	Limit int `json:"limit"`
+
+	// The offset from where to start looking.
+	// in: query
+	Offset int `json:"offset"`
+
+	// The name of the clients to filter by.
+	// in: query
+	Name string `json:"client_name"`
+
+	// The owner of the clients to filter by.
+	// in: query
+	Owner string `json:"owner"`
+}
+
 type Manager interface {
 	Storage
 

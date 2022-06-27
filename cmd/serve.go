e78bed63fffed9ff770186645a2931bfbd045623
aeneasr <3372410+aeneasr@users.noreply.github.com>
2022-06-27 15:11:49 +0200

    refactor: deprecate `--dangerous-allow-insecure-redirect-url` flag
    BREAKING CHANGE: CLI flag `--dangerous-allow-insecure-redirect-url` has been removed. Please use the `--dev` flag instead!

--- cmd/serve.go
+++ cmd/serve.go
@@ -60,7 +60,6 @@ All sub-commands share command line flags and configuration options.
 
 	configx.RegisterFlags(cmd.PersistentFlags())
 	cmd.PersistentFlags().Bool("dangerous-force-http", false, "DO NOT USE THIS IN PRODUCTION - Disables HTTP/2 over TLS (HTTPS) and serves HTTP instead")
-	cmd.PersistentFlags().StringSlice("dangerous-allow-insecure-redirect-urls", []string{}, "DO NOT USE THIS IN PRODUCTION - Disable HTTPS enforcement for the provided redirect URLs")
 	cmd.PersistentFlags().Bool("sqa-opt-out", false, "Disable anonymized telemetry reports - for more information please visit https://www.ory.sh/docs/ecosystem/sqa")
 
 	return cmd

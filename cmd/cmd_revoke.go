f3e1e0b2b6df28df5e7efa22412a9a58244a6191
aeneasr <3372410+aeneasr@users.noreply.github.com>
2022-06-27 11:45:32 +0200

    refactor: `hydra token revoke` command
    BREAKING CHANGE: The `hydra token revoke` command has been renamed to `hydra revoke token` and now supports structured output (JSON, tables, ...).

---
+++ cmd/cmd_revoke.go
@@ -0,0 +1,19 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+
+	"github.com/ory/x/cmdx"
+)
+
+func NewRevokeCmd(root *cobra.Command) *cobra.Command {
+	var cmd = &cobra.Command{
+		Use:     "revoke",
+		Aliases: []string{"ls"},
+		Short:   "Revoke resources",
+	}
+	cmd.AddCommand(NewRevokeTokenCmd(root))
+	cmdx.RegisterHTTPClientFlags(cmd.PersistentFlags())
+	cmdx.RegisterFormatFlags(cmd.PersistentFlags())
+	return cmd
+}

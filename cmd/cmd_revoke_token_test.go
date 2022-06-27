8ddfa6d272d0aa75b0229e9ca07e70b61f7addad
aeneasr <3372410+aeneasr@users.noreply.github.com>
2022-06-27 11:45:32 +0200

    refactor: `hydra token revoke` command
    BREAKING CHANGE: The `hydra token revoke` command has been renamed to `hydra revoke token` and now supports structured output (JSON, tables, ...).

---
+++ cmd/cmd_revoke_token_test.go
@@ -0,0 +1,48 @@
+package cmd_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/spf13/cobra"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/tidwall/gjson"
+	"golang.org/x/oauth2/clientcredentials"
+
+	"github.com/ory/hydra/cmd"
+	"github.com/ory/x/cmdx"
+	"github.com/ory/x/snapshotx"
+)
+
+func TestRevokeToken(t *testing.T) {
+	c := cmd.NewRevokeTokenCmd(new(cobra.Command))
+	public, _, reg := setupRoutes(t, c)
+	require.NoError(t, c.Flags().Set(cmdx.FlagEndpoint, public.URL))
+
+	expected := createClientCredentialsClient(t, reg)
+	cc := clientcredentials.Config{
+		ClientID: expected.GetID(), ClientSecret: expected.Secret,
+		TokenURL: public.URL + "/oauth2/token",
+	}
+
+	t.Run("case=revokes valid token but without client credentials", func(t *testing.T) {
+		token, err := cc.Token(context.Background())
+		require.NoError(t, err)
+
+		snapshotx.SnapshotT(t, cmdx.ExecExpectedErr(t, c, token.AccessToken))
+	})
+
+	t.Run("case=revokes valid token", func(t *testing.T) {
+		token, err := cc.Token(context.Background())
+		require.NoError(t, err)
+
+		actual := gjson.Parse(cmdx.ExecNoErr(t, c, "--client-id", expected.GetID(), "--client-secret", expected.Secret, token.AccessToken))
+		assert.Equal(t, token.AccessToken, actual.String())
+	})
+
+	t.Run("case=checks invalid token", func(t *testing.T) {
+		actual := gjson.Parse(cmdx.ExecNoErr(t, c, "invalid-token"))
+		assert.Equal(t, "invalid-token", actual.String())
+	})
+}

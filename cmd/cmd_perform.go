017b2fe116f57a9b808420a44b73d7aac209c689
aeneasr <3372410+aeneasr@users.noreply.github.com>
2022-06-27 11:46:07 +0200

    fix: remove incorrect aliases

--- cmd/cmd_perform.go
+++ cmd/cmd_perform.go
@@ -8,9 +8,8 @@ import (
 
 func NewPerformCmd(root *cobra.Command) *cobra.Command {
 	var cmd = &cobra.Command{
-		Use:     "perform",
-		Aliases: []string{"ls"},
-		Short:   "Perform OAuth 2.0 Flows",
+		Use:   "perform",
+		Short: "Perform OAuth 2.0 Flows",
 	}
 	cmd.AddCommand(NewPerformClientCredentialsCmd(root))
 	cmdx.RegisterHTTPClientFlags(cmd.PersistentFlags())

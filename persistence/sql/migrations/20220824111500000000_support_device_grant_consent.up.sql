8cab63d19cf00a0dbf88669a934a9592bc5e8eaa
Romain Caire <romain.caire@shadow.tech>
2022-08-31 19:00:28 +0200

    Update wrong database!

--- persistence/sql/migrations/20220824111500000000_support_device_grant_consent.up.sql
+++ persistence/sql/migrations/20220824111500000000_support_device_grant_consent.up.sql
@@ -1,15 +1,15 @@
 CREATE TABLE hydra_oauth2_device_grant_request
 (
     challenge             VARCHAR(40)  NOT NULL PRIMARY KEY,
-    requested_scope       TEXT          NOT NULL UNIQUE,
-    verifier              VARCHAR(40)   NOT NULL UNIQUE,
-    client_id             VARCHAR(255)  NOT NULL REFERENCES hydra_client (id) ON DELETE CASCADE,
-    requested_audience    VARCHAR(255)  NULL     DEFAULT '',
-    csrf                  VARCHAR(40)   NOT NULL,
-    device_code           VARCHAR(100)  NOT NULL,
-    user_code             VARCHAR(16)   NOT NULL,
-    accepted              BOOL          NOT NULL DEFAULT true,
-    accepted_at           TIMESTAMP     NULL
+    requested_scope       TEXT         NOT NULL,
+    verifier              VARCHAR(40)  NOT NULL UNIQUE,
+    client_id             VARCHAR(255) DEFAULT NULL,
+    requested_audience    VARCHAR(255) NULL     DEFAULT '',
+    csrf                  VARCHAR(40)  NOT NULL,
+    device_code           VARCHAR(100) NOT NULL,
+    user_code             VARCHAR(16)  NOT NULL,
+    accepted              BOOL         NOT NULL DEFAULT true,
+    accepted_at           TIMESTAMP    NULL
 );
 CREATE INDEX hydra_oauth2_device_grant_request_client_id_idx ON hydra_oauth2_device_grant_request (client_id);
 CREATE INDEX hydra_oauth2_device_grant_request_verifier_idx ON hydra_oauth2_device_grant_request (verifier);

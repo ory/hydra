40603113954d39d5ddf99fdc51bf64110f2ae885
Romain Caire <romain.caire@shadow.tech>
2022-09-09 12:21:03 +0200

    Add support for CockroachDB

---
+++ persistence/sql/migrations/20220824111500000000_support_device_grant_consent.cockroach.up.sql
@@ -0,0 +1,16 @@
+CREATE TABLE hydra_oauth2_device_grant_request
+(
+    challenge             VARCHAR(40)  NOT NULL PRIMARY KEY,
+    requested_scope       TEXT         NOT NULL,
+    verifier              VARCHAR(40)  NOT NULL UNIQUE,
+    client_id             UUID         NULL REFERENCES hydra_client (pk) ON DELETE CASCADE,
+    request_url           TEXT         NOT NULL,
+    requested_audience    VARCHAR(255) NULL     DEFAULT '',
+    csrf                  VARCHAR(40)  NOT NULL,
+    device_code_signature VARCHAR(255) NULL,
+    accepted              BOOL         NOT NULL DEFAULT true,
+    accepted_at           TIMESTAMP    NULL
+);
+CREATE INDEX hydra_oauth2_device_grant_request_client_id_idx ON hydra_oauth2_device_grant_request (client_id);
+CREATE INDEX hydra_oauth2_device_grant_request_verifier_idx ON hydra_oauth2_device_grant_request (verifier);
+CREATE INDEX hydra_oauth2_device_grant_request_challenge_idx ON hydra_oauth2_device_grant_request (challenge);

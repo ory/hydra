f22046fcee445dbc0b8c8bc49a9eb053ed485dab
aeneasr <3372410+aeneasr@users.noreply.github.com>
2022-11-10 16:29:39 +0100

    fix: isolate transactions for crdb

---
+++ persistence/sql/migrations/20221109000010000001_fix_foreign_key.cockroach.up.sql
@@ -0,0 +1,2 @@
+ALTER TABLE hydra_oauth2_flow ADD CONSTRAINT hydra_oauth2_flow_login_session_id_fk FOREIGN KEY (login_session_id) REFERENCES public.hydra_oauth2_authentication_session(id) ON DELETE SET NULL;
+ALTER TABLE hydra_oauth2_flow ALTER COLUMN login_session_id DROP DEFAULT;

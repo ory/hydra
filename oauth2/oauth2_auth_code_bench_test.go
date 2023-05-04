8a53697efdc19f0089c86acdc0de32e447985adc
Henning Perl <henning.perl@ory.sh>
2023-05-04 13:41:57 +0200

    benchmark just the flow

--- oauth2/oauth2_auth_code_bench_test.go
+++ oauth2/oauth2_auth_code_bench_test.go
@@ -6,24 +6,19 @@ package oauth2_test
 import (
 	"context"
 	"net/http"
-	"strconv"
 	"strings"
 	"testing"
-	"time"
 
 	hydra "github.com/ory/hydra-client-go/v2"
 	hc "github.com/ory/hydra/v2/client"
 	"github.com/ory/hydra/v2/driver/config"
 	"github.com/ory/hydra/v2/internal"
 	"github.com/ory/hydra/v2/internal/testhelpers"
-	"github.com/ory/hydra/v2/x"
 	"github.com/ory/x/contextx"
 	"github.com/ory/x/pointerx"
-	"github.com/ory/x/requirex"
 	"github.com/pborman/uuid"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"github.com/tidwall/gjson"
 	"go.opentelemetry.io/otel/sdk/trace"
 	"go.opentelemetry.io/otel/sdk/trace/tracetest"
 	"golang.org/x/oauth2"
@@ -36,6 +31,7 @@ func BenchmarkAuthCode(b *testing.B) {
 	tracer := trace.NewTracerProvider(trace.WithSpanProcessor(spans)).Tracer("")
 
 	dsn := "postgres://postgres:secret@127.0.0.1:3445/postgres?sslmode=disable"
+	//dsn := "memory"
 	reg := internal.NewRegistrySQLFromURL(b, dsn, true, new(contextx.Default)).WithTracer(tracer)
 	reg.Config().MustSet(ctx, config.KeyLogLevel, "error")
 	reg.Config().MustSet(ctx, config.KeyAccessTokenStrategy, "opaque")
@@ -156,97 +152,99 @@ func BenchmarkAuthCode(b *testing.B) {
 		}
 	}
 
-	assertRefreshToken := func(b *testing.B, token *oauth2.Token, c *oauth2.Config, expectedExp time.Time) {
-		actualExp, err := strconv.ParseInt(testhelpers.IntrospectToken(b, c, token.RefreshToken, adminTS).Get("exp").String(), 10, 64)
-		require.NoError(b, err)
-		requirex.EqualTime(b, expectedExp, time.Unix(actualExp, 0), time.Second)
-	}
-
-	assertIDToken := func(b *testing.B, token *oauth2.Token, c *oauth2.Config, expectedSubject, expectedNonce string, expectedExp time.Time) gjson.Result {
-		idt, ok := token.Extra("id_token").(string)
-		require.True(b, ok)
-		assert.NotEmpty(b, idt)
-
-		body, err := x.DecodeSegment(strings.Split(idt, ".")[1])
-		require.NoError(b, err)
-
-		claims := gjson.ParseBytes(body)
-		assert.True(b, time.Now().After(time.Unix(claims.Get("iat").Int(), 0)), "%s", claims)
-		assert.True(b, time.Now().After(time.Unix(claims.Get("nbf").Int(), 0)), "%s", claims)
-		assert.True(b, time.Now().Before(time.Unix(claims.Get("exp").Int(), 0)), "%s", claims)
-		requirex.EqualTime(b, expectedExp, time.Unix(claims.Get("exp").Int(), 0), 2*time.Second)
-		assert.NotEmpty(b, claims.Get("jti").String(), "%s", claims)
-		assert.EqualValues(b, reg.Config().IssuerURL(ctx).String(), claims.Get("iss").String(), "%s", claims)
-		assert.NotEmpty(b, claims.Get("sid").String(), "%s", claims)
-		assert.Equal(b, "1", claims.Get("acr").String(), "%s", claims)
-		require.Len(b, claims.Get("amr").Array(), 1, "%s", claims)
-		assert.EqualValues(b, "pwd", claims.Get("amr").Array()[0].String(), "%s", claims)
-
-		require.Len(b, claims.Get("aud").Array(), 1, "%s", claims)
-		assert.EqualValues(b, c.ClientID, claims.Get("aud").Array()[0].String(), "%s", claims)
-		assert.EqualValues(b, expectedSubject, claims.Get("sub").String(), "%s", claims)
-		assert.EqualValues(b, expectedNonce, claims.Get("nonce").String(), "%s", claims)
-		assert.EqualValues(b, `baz`, claims.Get("bar").String(), "%s", claims)
-
-		return claims
-	}
-
-	introspectAccessToken := func(b *testing.B, conf *oauth2.Config, token *oauth2.Token, expectedSubject string) gjson.Result {
-		require.NotEmpty(b, token.AccessToken)
-		i := testhelpers.IntrospectToken(b, conf, token.AccessToken, adminTS)
-		assert.True(b, i.Get("active").Bool(), "%s", i)
-		assert.EqualValues(b, conf.ClientID, i.Get("client_id").String(), "%s", i)
-		assert.EqualValues(b, expectedSubject, i.Get("sub").String(), "%s", i)
-		assert.EqualValues(b, `bar`, i.Get("ext.foo").String(), "%s", i)
-		return i
-	}
-
-	assertJWTAccessToken := func(b *testing.B, strat string, conf *oauth2.Config, token *oauth2.Token, expectedSubject string, expectedExp time.Time) gjson.Result {
-		require.NotEmpty(b, token.AccessToken)
-		parts := strings.Split(token.AccessToken, ".")
-		if strat != "jwt" {
-			require.Len(b, parts, 2)
-			return gjson.Parse("null")
-		}
-		require.Len(b, parts, 3)
-
-		body, err := x.DecodeSegment(parts[1])
-		require.NoError(b, err)
-
-		i := gjson.ParseBytes(body)
-		assert.NotEmpty(b, i.Get("jti").String())
-		assert.EqualValues(b, conf.ClientID, i.Get("client_id").String(), "%s", i)
-		assert.EqualValues(b, expectedSubject, i.Get("sub").String(), "%s", i)
-		assert.EqualValues(b, reg.Config().IssuerURL(ctx).String(), i.Get("iss").String(), "%s", i)
-		assert.True(b, time.Now().After(time.Unix(i.Get("iat").Int(), 0)), "%s", i)
-		assert.True(b, time.Now().After(time.Unix(i.Get("nbf").Int(), 0)), "%s", i)
-		assert.True(b, time.Now().Before(time.Unix(i.Get("exp").Int(), 0)), "%s", i)
-		requirex.EqualTime(b, expectedExp, time.Unix(i.Get("exp").Int(), 0), time.Second)
-		assert.EqualValues(b, `bar`, i.Get("ext.foo").String(), "%s", i)
-		assert.EqualValues(b, `["hydra","offline","openid"]`, i.Get("scp").Raw, "%s", i)
-		return i
-	}
+	//assertRefreshToken := func(b *testing.B, token *oauth2.Token, c *oauth2.Config, expectedExp time.Time) {
+	//	actualExp, err := strconv.ParseInt(testhelpers.IntrospectToken(b, c, token.RefreshToken, adminTS).Get("exp").String(), 10, 64)
+	//	require.NoError(b, err)
+	//	requirex.EqualTime(b, expectedExp, time.Unix(actualExp, 0), time.Second)
+	//}
+	//
+	//assertIDToken := func(b *testing.B, token *oauth2.Token, c *oauth2.Config, expectedSubject, expectedNonce string, expectedExp time.Time) gjson.Result {
+	//	idt, ok := token.Extra("id_token").(string)
+	//	require.True(b, ok)
+	//	assert.NotEmpty(b, idt)
+	//
+	//	body, err := x.DecodeSegment(strings.Split(idt, ".")[1])
+	//	require.NoError(b, err)
+	//
+	//	claims := gjson.ParseBytes(body)
+	//	assert.True(b, time.Now().After(time.Unix(claims.Get("iat").Int(), 0)), "%s", claims)
+	//	assert.True(b, time.Now().After(time.Unix(claims.Get("nbf").Int(), 0)), "%s", claims)
+	//	assert.True(b, time.Now().Before(time.Unix(claims.Get("exp").Int(), 0)), "%s", claims)
+	//	requirex.EqualTime(b, expectedExp, time.Unix(claims.Get("exp").Int(), 0), 2*time.Second)
+	//	assert.NotEmpty(b, claims.Get("jti").String(), "%s", claims)
+	//	assert.EqualValues(b, reg.Config().IssuerURL(ctx).String(), claims.Get("iss").String(), "%s", claims)
+	//	assert.NotEmpty(b, claims.Get("sid").String(), "%s", claims)
+	//	assert.Equal(b, "1", claims.Get("acr").String(), "%s", claims)
+	//	require.Len(b, claims.Get("amr").Array(), 1, "%s", claims)
+	//	assert.EqualValues(b, "pwd", claims.Get("amr").Array()[0].String(), "%s", claims)
+	//
+	//	require.Len(b, claims.Get("aud").Array(), 1, "%s", claims)
+	//	assert.EqualValues(b, c.ClientID, claims.Get("aud").Array()[0].String(), "%s", claims)
+	//	assert.EqualValues(b, expectedSubject, claims.Get("sub").String(), "%s", claims)
+	//	assert.EqualValues(b, expectedNonce, claims.Get("nonce").String(), "%s", claims)
+	//	assert.EqualValues(b, `baz`, claims.Get("bar").String(), "%s", claims)
+	//
+	//	return claims
+	//}
+	//
+	//introspectAccessToken := func(b *testing.B, conf *oauth2.Config, token *oauth2.Token, expectedSubject string) gjson.Result {
+	//	require.NotEmpty(b, token.AccessToken)
+	//	i := testhelpers.IntrospectToken(b, conf, token.AccessToken, adminTS)
+	//	assert.True(b, i.Get("active").Bool(), "%s", i)
+	//	assert.EqualValues(b, conf.ClientID, i.Get("client_id").String(), "%s", i)
+	//	assert.EqualValues(b, expectedSubject, i.Get("sub").String(), "%s", i)
+	//	assert.EqualValues(b, `bar`, i.Get("ext.foo").String(), "%s", i)
+	//	return i
+	//}
+	//
+	//assertJWTAccessToken := func(b *testing.B, strat string, conf *oauth2.Config, token *oauth2.Token, expectedSubject string, expectedExp time.Time) gjson.Result {
+	//	require.NotEmpty(b, token.AccessToken)
+	//	parts := strings.Split(token.AccessToken, ".")
+	//	if strat != "jwt" {
+	//		require.Len(b, parts, 2)
+	//		return gjson.Parse("null")
+	//	}
+	//	require.Len(b, parts, 3)
+	//
+	//	body, err := x.DecodeSegment(parts[1])
+	//	require.NoError(b, err)
+	//
+	//	i := gjson.ParseBytes(body)
+	//	assert.NotEmpty(b, i.Get("jti").String())
+	//	assert.EqualValues(b, conf.ClientID, i.Get("client_id").String(), "%s", i)
+	//	assert.EqualValues(b, expectedSubject, i.Get("sub").String(), "%s", i)
+	//	assert.EqualValues(b, reg.Config().IssuerURL(ctx).String(), i.Get("iss").String(), "%s", i)
+	//	assert.True(b, time.Now().After(time.Unix(i.Get("iat").Int(), 0)), "%s", i)
+	//	assert.True(b, time.Now().After(time.Unix(i.Get("nbf").Int(), 0)), "%s", i)
+	//	assert.True(b, time.Now().Before(time.Unix(i.Get("exp").Int(), 0)), "%s", i)
+	//	requirex.EqualTime(b, expectedExp, time.Unix(i.Get("exp").Int(), 0), time.Second)
+	//	assert.EqualValues(b, `bar`, i.Get("ext.foo").String(), "%s", i)
+	//	assert.EqualValues(b, `["hydra","offline","openid"]`, i.Get("scp").Raw, "%s", i)
+	//	return i
+	//}
 
 	subject := "aeneas-rekkas"
 	nonce := uuid.New()
+	c, conf := newOAuth2Client(b, testhelpers.NewCallbackURL(b, "callback", testhelpers.HTTPServerNotImplementedHandler))
+
+	testhelpers.NewLoginConsentUI(b, reg.Config(),
+		acceptLoginHandler(b, c, subject, nil),
+		acceptConsentHandler(b, c, subject, nil),
+	)
 
 	run := func(b *testing.B, strategy string) {
-		c, conf := newOAuth2Client(b, testhelpers.NewCallbackURL(b, "callback", testhelpers.HTTPServerNotImplementedHandler))
-		testhelpers.NewLoginConsentUI(b, reg.Config(),
-			acceptLoginHandler(b, c, subject, nil),
-			acceptConsentHandler(b, c, subject, nil),
-		)
-
+		//pop.Debug = true
 		code, _ := getAuthorizeCode(b, conf, nil, oauth2.SetAuthURLParam("nonce", nonce))
+		//pop.Debug = false
 		require.NotEmpty(b, code)
-		token, err := conf.Exchange(context.Background(), code)
-		iat := time.Now()
+		_, err := conf.Exchange(context.Background(), code)
+		//iat := time.Now()
 		require.NoError(b, err)
 
-		introspectAccessToken(b, conf, token, subject)
-		assertJWTAccessToken(b, strategy, conf, token, subject, iat.Add(reg.Config().GetAccessTokenLifespan(ctx)))
-		assertIDToken(b, token, conf, subject, nonce, iat.Add(reg.Config().GetIDTokenLifespan(ctx)))
-		assertRefreshToken(b, token, conf, iat.Add(reg.Config().GetRefreshTokenLifespan(ctx)))
+		//introspectAccessToken(b, conf, token, subject)
+		//assertJWTAccessToken(b, strategy, conf, token, subject, iat.Add(reg.Config().GetAccessTokenLifespan(ctx)))
+		//assertIDToken(b, token, conf, subject, nonce, iat.Add(reg.Config().GetIDTokenLifespan(ctx)))
+		//assertRefreshToken(b, token, conf, iat.Add(reg.Config().GetRefreshTokenLifespan(ctx)))
 	}
 
 	b.Run("strategy=jwt", func(b *testing.B) {

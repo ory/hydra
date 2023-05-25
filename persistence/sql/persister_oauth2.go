ae5339b8cad4fe8dec7ed64eb791a53cefecacdf
Arne Luenser <arne.luenser@ory.sh>
2023-05-25 11:12:30 +0200

    fix: persist flow on success

--- persistence/sql/persister_oauth2.go
+++ persistence/sql/persister_oauth2.go
@@ -20,7 +20,9 @@ import (
 
 	"github.com/ory/fosite"
 	"github.com/ory/fosite/storage"
+	"github.com/ory/hydra/v2/flow"
 	"github.com/ory/hydra/v2/oauth2"
+	"github.com/ory/hydra/v2/oauth2/flowctx"
 	"github.com/ory/x/errorsx"
 	"github.com/ory/x/sqlcon"
 	"github.com/ory/x/stringsx"
@@ -227,17 +229,17 @@ func (p *Persister) createSession(ctx context.Context, signature string, request
 	ctx, span := p.r.Tracer(ctx).Tracer().Start(ctx, "persistence.sql.createSession")
 	defer span.End()
 
-	//v, err := flowctx.ValueFromCtx(ctx, flowctx.FlowCookie)
-	//if err == nil {
-	//	v.PersistOnce.Do(func() {
-	//		err = sqlcon.HandleError(p.Connection(ctx).Create(v.Ptr.(*flow.Flow)))
-	//	})
-	//	if err != nil {
-	//		return err
-	//	}
-	//} else if !errors.Is(err, flowctx.ErrNoValueInCtx) {
-	//	return err
-	//}
+	v, err := flowctx.ValueFromCtx(ctx, flowctx.FlowCookie)
+	if err == nil {
+		v.PersistOnce.Do(func() {
+			err = sqlcon.HandleError(p.Connection(ctx).Create(v.Ptr.(*flow.Flow)))
+		})
+		if err != nil {
+			return err
+		}
+	} else if !errors.Is(err, flowctx.ErrNoValueInCtx) {
+		return err
+	}
 
 	req, err := p.sqlSchemaFromRequest(ctx, signature, requester, table)
 	if err != nil {

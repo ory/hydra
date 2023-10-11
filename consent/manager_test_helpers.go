f3b99b1670485273cb3963c591a43320973ca8c8
Henning Perl <henning.perl@ory.sh>
2023-10-11 10:09:46 +0200

    test: add test for consent double-submit

--- consent/manager_test_helpers.go
+++ consent/manager_test_helpers.go
@@ -547,6 +547,11 @@ func ManagerTests(deps Deps, m Manager, clientManager client.Manager, fositeMana
 					assert.Equal(t, consentRequest.ID, got2.ID)
 					assert.Equal(t, h.GrantedAudience, got2.GrantedAudience)
 
+					t.Run("subcase=detect double-submit for consent verifier", func(t *testing.T) {
+						_, err := m.VerifyAndInvalidateConsentRequest(ctx, consentVerifier)
+						require.Error(t, err)
+					})
+
 					if tc.hasError {
 						assert.True(t, got2.HasError())
 					}

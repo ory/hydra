23c8dce28da1f82e4da13fe06b293475b3499f42
Nikos <nikos.sklikas@canonical.com>
2024-04-15 14:02:19 +0300

    fix: add GetDeviceCodeSessionByRequestID method

--- x/fosite_storer.go
+++ x/fosite_storer.go
@@ -46,6 +46,7 @@ type FositeStorer interface {
 	// This is duplicated from Ory Fosite to help against deprecation linting errors.
 	DeleteOpenIDConnectSession(ctx context.Context, authorizeCode string) error
 
+	GetDeviceCodeSessionByRequestID(ctx context.Context, requestID string, requester fosite.Session) (fosite.Requester, error)
 	UpdateDeviceCodeSessionByRequestID(ctx context.Context, requestID string, requester fosite.Requester) error
 	UpdateAndInvalidateUserCodeSessionByRequestID(ctx context.Context, signature, request_id string) (err error)
 }

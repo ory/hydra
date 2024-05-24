3926c7fa9a744ec587ce4e6f6c251b2274321c61
lets-get-this-bread-2019 <56768350+lets-get-this-bread-2019@users.noreply.github.com>
2024-05-23 23:41:53 -0700

    Fix table names in sql migration file

--- persistence/sql/migrations/20240403121110000001_add_expire_columns.up.sql
+++ persistence/sql/migrations/20240403121110000001_add_expire_columns.up.sql
@@ -5,7 +5,7 @@ ALTER TABLE hydra_oauth2_code ADD COLUMN expires_at TIMESTAMP NULL;
 ALTER TABLE hydra_oauth2_pkce ADD COLUMN expires_at TIMESTAMP NULL;
 
 CREATE INDEX hydra_oauth2_oidc_expires_at_idx ON hydra_oauth2_oidc (expires_at);
-CREATE INDEX hydra_oauth2_access_expires_at_idx ON hydra_oauth2_oidc (expires_at);
-CREATE INDEX hydra_oauth2_refresh_expires_at_idx ON hydra_oauth2_oidc (expires_at);
-CREATE INDEX hydra_oauth2_code_expires_at_idx ON hydra_oauth2_oidc (expires_at);
-CREATE INDEX hydra_oauth2_pkce_expires_at_idx ON hydra_oauth2_oidc (expires_at);
+CREATE INDEX hydra_oauth2_access_expires_at_idx ON hydra_oauth2_access (expires_at);
+CREATE INDEX hydra_oauth2_refresh_expires_at_idx ON hydra_oauth2_refresh (expires_at);
+CREATE INDEX hydra_oauth2_code_expires_at_idx ON hydra_oauth2_code (expires_at);
+CREATE INDEX hydra_oauth2_pkce_expires_at_idx ON hydra_oauth2_pkce (expires_at);

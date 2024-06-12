324bd618e89b6fffe560022eada6a4ecfe0a7abd
aeneasr <3372410+aeneasr@users.noreply.github.com>
2024-06-12 22:23:05 +0200

    fix: incorrect indices

---
+++ persistence/sql/migrations/20240612222110000001_add_expire_columns.down.sql
@@ -0,0 +1,9 @@
+DROP INDEX hydra_oauth2_access_expires_at_idx;
+DROP INDEX hydra_oauth2_refresh_expires_at_idx;
+DROP INDEX hydra_oauth2_code_expires_at_idx;
+DROP INDEX hydra_oauth2_pkce_expires_at_idx;
+
+CREATE INDEX hydra_oauth2_access_expires_at_idx ON hydra_oauth2_oidc (expires_at);
+CREATE INDEX hydra_oauth2_refresh_expires_at_idx ON hydra_oauth2_oidc (expires_at);
+CREATE INDEX hydra_oauth2_code_expires_at_idx ON hydra_oauth2_oidc (expires_at);
+CREATE INDEX hydra_oauth2_pkce_expires_at_idx ON hydra_oauth2_oidc (expires_at);

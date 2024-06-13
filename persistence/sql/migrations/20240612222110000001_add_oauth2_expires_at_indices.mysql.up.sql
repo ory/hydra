62cf316d4a0070e4e033d41274fd4d02fc4bf143
aeneasr <3372410+aeneasr@users.noreply.github.com>
2024-06-13 10:08:11 +0200

    chore: synchronize workspaces

---
+++ persistence/sql/migrations/20240612222110000001_add_oauth2_expires_at_indices.mysql.up.sql
@@ -0,0 +1,9 @@
+CREATE INDEX hydra_oauth2_access_expires_at_v2_idx ON hydra_oauth2_access (expires_at);
+CREATE INDEX hydra_oauth2_refresh_expires_at_v2_idx ON hydra_oauth2_refresh (expires_at);
+CREATE INDEX hydra_oauth2_code_expires_at_v2_idx ON hydra_oauth2_code (expires_at);
+CREATE INDEX hydra_oauth2_pkce_expires_at_v2_idx ON hydra_oauth2_pkce (expires_at);
+
+DROP INDEX hydra_oauth2_access_expires_at_idx ON hydra_oauth2_oidc;
+DROP INDEX hydra_oauth2_refresh_expires_at_idx ON hydra_oauth2_oidc;
+DROP INDEX hydra_oauth2_code_expires_at_idx ON hydra_oauth2_oidc;
+DROP INDEX hydra_oauth2_pkce_expires_at_idx ON hydra_oauth2_oidc;

522e5470ad3bde55c29b18c37135213aa7eec98c
Trevor Foster <trevorfoster19@gmail.com>
2025-01-31 14:14:28 -0500

    Fix format

--- jwk/helper_test.go
+++ jwk/helper_test.go
@@ -29,7 +29,6 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
-	"github.com/ory/hydra/v2/internal"
 	"github.com/ory/hydra/v2/jwk"
 	"github.com/ory/hydra/v2/x"
 	"github.com/ory/x/contextx"

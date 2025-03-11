1f8d416bfca856e526a26db21984b5bee20ceb26
Giulio Calzolari <9049490+giuliocalzolari@users.noreply.github.com>
2025-03-11 20:58:16 +0100

    Update 20211011000001000000_change_jwk_primary_key.postgres.up.sql

--- persistence/sql/migrations/20211011000001000000_change_jwk_primary_key.postgres.up.sql
+++ persistence/sql/migrations/20211011000001000000_change_jwk_primary_key.postgres.up.sql
@@ -1,14 +1,12 @@
 ALTER TABLE hydra_jwk RENAME pk TO pk_deprecated;
 -- UUID generation based on https://stackoverflow.com/a/21327318/12723442
-ALTER TABLE hydra_jwk ADD COLUMN pk UUID DEFAULT uuid_in(
-  overlay(
+ALTER TABLE hydra_jwk ADD COLUMN pk UUID DEFAULT (
+  uuid_in(
     overlay(
-      md5(random()::text || ':' || clock_timestamp()::text)
-      placing '4'
-      from 13
+      gen_random_uuid()::text
+      placing '4' from 13
     )
-    placing to_hex(floor(random()*(11-8+1) + 8)::int)::text
-    from 17
+    placing to_hex(floor(random() * (11 - 8 + 1) + 8)::int)::text from 17
   )::cstring
 );
 ALTER TABLE hydra_jwk ALTER pk DROP DEFAULT;

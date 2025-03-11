3bcce6a2950fa28f3f1897d7bfd6378b63640dbb
Giulio Calzolari <9049490+giuliocalzolari@users.noreply.github.com>
2025-03-11 20:59:05 +0100

    Update 20150101000001000000_networks.postgres.up.sql

--- persistence/sql/src/20150101000001_networks/20150101000001000000_networks.postgres.up.sql
+++ persistence/sql/src/20150101000001_networks/20150101000001000000_networks.postgres.up.sql
@@ -5,14 +5,14 @@ CREATE TABLE "networks" (
   "updated_at" timestamp NOT NULL
 );
 
-INSERT INTO networks (id, created_at, updated_at) VALUES (uuid_in(
-  overlay(
+INSERT INTO networks (id, created_at, updated_at) VALUES (
+  uuid_in(
     overlay(
-      md5(random()::text || ':' || clock_timestamp()::text)
-      placing '4'
-      from 13
+      gen_random_uuid()::text
+      placing '4' from 13
     )
-    placing to_hex(floor(random()*(11-8+1) + 8)::int)::text
-    from 17
-  )::cstring
-), '2013-10-07 08:23:19', '2013-10-07 08:23:19');
+    placing to_hex(floor(random() * (11 - 8 + 1) + 8)::int)::text from 17
+  )::cstring,
+  '2013-10-07 08:23:19',
+  '2013-10-07 08:23:19'
+);

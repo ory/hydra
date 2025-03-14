25eda61c41f33b7b8761235501357701e0e822e2
Giulio Calzolari <9049490+giuliocalzolari@users.noreply.github.com>
2025-03-14 10:01:43 +0100

    Update 20211004110001000000_change_client_primary_key.postgres.up.sql

--- persistence/sql/migrations/20211004110001000000_change_client_primary_key.postgres.up.sql
+++ persistence/sql/migrations/20211004110001000000_change_client_primary_key.postgres.up.sql
@@ -1,12 +1,14 @@
 ALTER TABLE hydra_client RENAME pk TO pk_deprecated;
 -- UUID generation based on https://stackoverflow.com/a/21327318/12723442
-ALTER TABLE hydra_client ADD COLUMN pk UUID DEFAULT (
-  uuid_in(
+ALTER TABLE hydra_client ADD COLUMN pk UUID DEFAULT uuid_in(
+  overlay(
     overlay(
-      gen_random_uuid()::text
-      placing '4' from 13
+      substr(to_hex(random()::numeric || random()::numeric), 1, 32)
+      placing '4'
+      from 13
     )
-    placing to_hex(floor(random() * (11 - 8 + 1) + 8)::int)::text from 17
+    placing to_hex(floor(random() * 4 + 8)::int)::text
+    from 17
   )::cstring
 );
 ALTER TABLE hydra_client ALTER pk DROP DEFAULT;

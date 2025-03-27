1c7aa52a24d1627439a241cf5b1bd57c4fa71bda
Giulio Calzolari <9049490+giuliocalzolari@users.noreply.github.com>
2025-03-27 11:31:01 +0100

    Update 20211004110001000000_change_client_primary_key.postgres.up.sql

--- persistence/sql/migrations/20211004110001000000_change_client_primary_key.postgres.up.sql
+++ persistence/sql/migrations/20211004110001000000_change_client_primary_key.postgres.up.sql
@@ -1,5 +1,6 @@
 ALTER TABLE hydra_client RENAME pk TO pk_deprecated;
-ALTER TABLE hydra_client ADD COLUMN pk UUID DEFAULT gen_random_uuid();
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+ALTER TABLE hydra_client ADD COLUMN pk UUID DEFAULT uuid_generate_v4();
 ALTER TABLE hydra_client ALTER pk DROP DEFAULT;
 ALTER TABLE hydra_client DROP CONSTRAINT hydra_client_pkey;
 ALTER TABLE hydra_client ADD PRIMARY KEY (pk);

a6de797d8f75fc96f63b2d50ecf6eafb65f7946c
Giulio Calzolari <9049490+giuliocalzolari@users.noreply.github.com>
2025-03-27 11:31:42 +0100

    Update 20211011000001000000_change_jwk_primary_key.postgres.up.sql

--- persistence/sql/migrations/20211011000001000000_change_jwk_primary_key.postgres.up.sql
+++ persistence/sql/migrations/20211011000001000000_change_jwk_primary_key.postgres.up.sql
@@ -1,5 +1,6 @@
 ALTER TABLE hydra_jwk RENAME pk TO pk_deprecated;
-ALTER TABLE hydra_jwk ADD COLUMN pk UUID DEFAULT gen_random_uuid();
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+ALTER TABLE hydra_jwk ADD COLUMN pk UUID DEFAULT uuid_generate_v4();
 ALTER TABLE hydra_jwk ALTER pk DROP DEFAULT;
 ALTER TABLE hydra_jwk DROP CONSTRAINT hydra_jwk_pkey;
 ALTER TABLE hydra_jwk ADD PRIMARY KEY (pk);

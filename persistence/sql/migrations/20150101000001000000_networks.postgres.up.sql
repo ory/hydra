c9119da3000ab123cf083406ae3094b535e0abc2
Giulio Calzolari <9049490+giuliocalzolari@users.noreply.github.com>
2025-03-27 11:29:05 +0100

    Update 20150101000001000000_networks.postgres.up.sql

--- persistence/sql/migrations/20150101000001000000_networks.postgres.up.sql
+++ persistence/sql/migrations/20150101000001000000_networks.postgres.up.sql
@@ -6,5 +6,5 @@ CREATE TABLE "networks" (
   "created_at" timestamp NOT NULL,
   "updated_at" timestamp NOT NULL
 );
-
-INSERT INTO networks (id, created_at, updated_at) VALUES (gen_random_uuid(), '2013-10-07 08:23:19', '2013-10-07 08:23:19');
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+INSERT INTO networks (id, created_at, updated_at) VALUES (uuid_generate_v4(), '2013-10-07 08:23:19', '2013-10-07 08:23:19');

c20606606654af975e5d82998956bb998acee576
Giulio Calzolari <9049490+giuliocalzolari@users.noreply.github.com>
2025-04-14 11:25:15 +0200

    fix: using uuid_generate_v4 function (#3958)
    Removing the md5 function for the uuid generation with native pgsql
    function https://www.postgresql.org/docs/current/uuid-ossp.html
    
    Closes https://github.com/ory/hydra/issues/3844

--- persistence/sql/src/20150101000001_networks/20150101000001000000_networks.postgres.up.sql
+++ persistence/sql/src/20150101000001_networks/20150101000001000000_networks.postgres.up.sql
@@ -4,15 +4,5 @@ CREATE TABLE "networks" (
   "created_at" timestamp NOT NULL,
   "updated_at" timestamp NOT NULL
 );
-
-INSERT INTO networks (id, created_at, updated_at) VALUES (uuid_in(
-  overlay(
-    overlay(
-      md5(random()::text || ':' || clock_timestamp()::text)
-      placing '4'
-      from 13
-    )
-    placing to_hex(floor(random()*(11-8+1) + 8)::int)::text
-    from 17
-  )::cstring
-), '2013-10-07 08:23:19', '2013-10-07 08:23:19');
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+INSERT INTO networks (id, created_at, updated_at) VALUES uuid_generate_v4(), '2013-10-07 08:23:19', '2013-10-07 08:23:19');

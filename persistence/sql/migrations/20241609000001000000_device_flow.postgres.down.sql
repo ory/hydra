c5af5f4bf8934a99b8e469aca3cbbf565e787479
Adam Wałach <adam.walach@gmail.com>
2025-05-09 15:58:22 +0200

    chore: fix down migrations for device flow

---
+++ persistence/sql/migrations/20241609000001000000_device_flow.postgres.down.sql
@@ -0,0 +1,17 @@
+ALTER TABLE hydra_oauth2_device_auth_codes DROP CONSTRAINT IF EXISTS hydra_oauth2_device_auth_codes_challenge_id_fk;
+ALTER TABLE hydra_oauth2_device_auth_codes DROP CONSTRAINT IF EXISTS hydra_oauth2_device_auth_codes_client_id_fk;
+ALTER TABLE hydra_oauth2_device_auth_codes DROP CONSTRAINT IF EXISTS hydra_oauth2_device_auth_codes_nid_fk_idx;
+
+DROP TABLE IF EXISTS hydra_oauth2_device_auth_codes;
+
+ALTER TABLE hydra_oauth2_flow DROP COLUMN IF EXISTS device_challenge_id;
+ALTER TABLE hydra_oauth2_flow DROP COLUMN IF EXISTS device_code_request_id;
+ALTER TABLE hydra_oauth2_flow DROP COLUMN IF EXISTS device_verifier;
+ALTER TABLE hydra_oauth2_flow DROP COLUMN IF EXISTS device_csrf;
+ALTER TABLE hydra_oauth2_flow DROP COLUMN IF EXISTS device_was_used;
+ALTER TABLE hydra_oauth2_flow DROP COLUMN IF EXISTS device_handled_at;
+ALTER TABLE hydra_oauth2_flow DROP COLUMN IF EXISTS device_error;
+
+ALTER TABLE hydra_client DROP COLUMN IF EXISTS device_authorization_grant_id_token_lifespan;
+ALTER TABLE hydra_client DROP COLUMN IF EXISTS device_authorization_grant_access_token_lifespan;
+ALTER TABLE hydra_client DROP COLUMN IF EXISTS device_authorization_grant_refresh_token_lifespan;

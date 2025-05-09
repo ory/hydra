b02e3e57675a78c421e38cbecd7470d2c6366293
Adam Wałach <adam.walach@gmail.com>
2025-05-09 16:07:36 +0200

    chore: revert mysql changes

--- persistence/sql/migrations/20241609000001000000_device_flow.down.sql
+++ persistence/sql/migrations/20241609000001000000_device_flow.down.sql
@@ -1,17 +1,13 @@
-ALTER TABLE hydra_oauth2_device_auth_codes DROP FOREIGN KEY hydra_oauth2_device_auth_codes_challenge_id_fk;
-ALTER TABLE hydra_oauth2_device_auth_codes DROP FOREIGN KEY hydra_oauth2_device_auth_codes_client_id_fk;
-ALTER TABLE hydra_oauth2_device_auth_codes DROP FOREIGN KEY hydra_oauth2_device_auth_codes_nid_fk_idx;
+ALTER TABLE hydra_oauth2_device_auth_codes DROP FOREIGN KEY IF EXISTS hydra_oauth2_device_auth_codes_challenge_id_fk;
+ALTER TABLE hydra_oauth2_device_auth_codes DROP FOREIGN KEY IF EXISTS hydra_oauth2_device_auth_codes_client_id_fk;
+ALTER TABLE hydra_oauth2_device_auth_codes DROP FOREIGN KEY IF EXISTS hydra_oauth2_device_auth_codes_nid_fk_idx;
 
-DROP TABLE hydra_oauth2_device_auth_codes;
+DROP TABLE IF EXISTS hydra_oauth2_device_auth_codes;
 
-ALTER TABLE hydra_oauth2_flow DROP COLUMN device_challenge_id;
-ALTER TABLE hydra_oauth2_flow DROP COLUMN device_code_request_id;
-ALTER TABLE hydra_oauth2_flow DROP COLUMN device_verifier;
-ALTER TABLE hydra_oauth2_flow DROP COLUMN device_csrf;
-ALTER TABLE hydra_oauth2_flow DROP COLUMN device_was_used;
-ALTER TABLE hydra_oauth2_flow DROP COLUMN device_handled_at;
-ALTER TABLE hydra_oauth2_flow DROP COLUMN device_error;
-
-ALTER TABLE hydra_client DROP COLUMN device_authorization_grant_id_token_lifespan;
-ALTER TABLE hydra_client DROP COLUMN device_authorization_grant_access_token_lifespan;
-ALTER TABLE hydra_client DROP COLUMN device_authorization_grant_refresh_token_lifespan;
+ALTER TABLE hydra_oauth2_flow DROP COLUMN IF EXISTS device_challenge_id;
+ALTER TABLE hydra_oauth2_flow DROP COLUMN IF EXISTS device_code_request_id;
+ALTER TABLE hydra_oauth2_flow DROP COLUMN IF EXISTS device_verifier;
+ALTER TABLE hydra_oauth2_flow DROP COLUMN IF EXISTS device_csrf;
+ALTER TABLE hydra_oauth2_flow DROP COLUMN IF EXISTS device_was_used;
+ALTER TABLE hydra_oauth2_flow DROP COLUMN IF EXISTS device_handled_at;
+ALTER TABLE hydra_oauth2_flow DROP COLUMN IF EXISTS device_error;

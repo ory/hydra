6b848fdd7491789e580211ecbab2d67840f09e3e
Adam Wałach <adam.walach@gmail.com>
2025-05-09 16:02:30 +0200

    chore: remove

--- persistence/sql/migrations/20241609000001000000_device_flow.down.sql
+++ persistence/sql/migrations/20241609000001000000_device_flow.down.sql
@@ -2,7 +2,7 @@ ALTER TABLE hydra_oauth2_device_auth_codes DROP FOREIGN KEY hydra_oauth2_device_
 ALTER TABLE hydra_oauth2_device_auth_codes DROP FOREIGN KEY hydra_oauth2_device_auth_codes_client_id_fk;
 ALTER TABLE hydra_oauth2_device_auth_codes DROP FOREIGN KEY hydra_oauth2_device_auth_codes_nid_fk_idx;
 
-DROP TABLE IF EXISTS hydra_oauth2_device_auth_codes;
+DROP TABLE hydra_oauth2_device_auth_codes;
 
 ALTER TABLE hydra_oauth2_flow DROP COLUMN device_challenge_id;
 ALTER TABLE hydra_oauth2_flow DROP COLUMN device_code_request_id;

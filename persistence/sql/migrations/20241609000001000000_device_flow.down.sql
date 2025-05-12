aecf1180cf2275b9ae34c1372f73c6c30b53b8b5
Adam Wałach <adam.walach@gmail.com>
2025-05-12 08:27:26 +0200

    chore: clean up drop foreign key

--- persistence/sql/migrations/20241609000001000000_device_flow.down.sql
+++ persistence/sql/migrations/20241609000001000000_device_flow.down.sql
@@ -1,7 +1,3 @@
-ALTER TABLE hydra_oauth2_device_auth_codes DROP FOREIGN KEY IF EXISTS hydra_oauth2_device_auth_codes_challenge_id_fk;
-ALTER TABLE hydra_oauth2_device_auth_codes DROP FOREIGN KEY IF EXISTS hydra_oauth2_device_auth_codes_client_id_fk;
-ALTER TABLE hydra_oauth2_device_auth_codes DROP FOREIGN KEY IF EXISTS hydra_oauth2_device_auth_codes_nid_fk_idx;
-
 DROP TABLE IF EXISTS hydra_oauth2_device_auth_codes;
 
 ALTER TABLE hydra_oauth2_flow DROP COLUMN IF EXISTS device_challenge_id;

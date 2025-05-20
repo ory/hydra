70104b1b5fd8fad8ef2505e15dbd4c8d446fa00c
Philippe Gaultier <philippe.gaultier@ory.sh>
2025-05-20 16:05:48 +0200

    comment

--- persistence/sql/migrations/20250520000001000000_add_oauth2_trusted_jwt_bearer_issuer_index.up.sql
+++ persistence/sql/migrations/20250520000001000000_add_oauth2_trusted_jwt_bearer_issuer_index.up.sql
@@ -1,1 +1,2 @@
+-- `key_id` is unique-ish per row so we place it first in the index to make queries including it very fast.
 CREATE UNIQUE INDEX hydra_oauth2_trusted_jwt_bearer_issuer_key_id ON hydra_oauth2_trusted_jwt_bearer_issuer (key_id ASC, issuer ASC, subject ASC, nid ASC);

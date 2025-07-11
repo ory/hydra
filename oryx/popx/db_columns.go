7e49133f435d6a0f74a63e8f8d03c5d314d7d3c6
Philippe Gaultier <philigaultier@gmail.com>
2025-07-11 08:41:40 +0200

    refactor: move database meta functions to root x folder for reusability
    GitOrigin-RevId: 30ee938ea5f1d19bac8967e0ebfe2d595ec27d2b

---
+++ oryx/popx/db_columns.go
@@ -0,0 +1,48 @@
+package popx
+
+import (
+	"github.com/ory/pop/v6"
+)
+
+func DBColumns[T any](quoter Quoter) string {
+	return (&pop.Model{Value: new(T)}).Columns().QuotedString(quoter)
+}
+
+// IndexHint returns the table name including the index hint, if the database
+// supports it.
+func IndexHint(conn *pop.Connection, table string, index string) string {
+	if conn.Dialect.Name() == "cockroach" {
+		return table + "@" + index
+	}
+	return table
+}
+
+func WritableDBColumnNames[T any]() []string {
+	var names []string
+	for _, c := range (&pop.Model{Value: new(T)}).Columns().Writeable().Cols {
+		names = append(names, c.Name)
+	}
+	return names
+}
+
+func DBColumnsExcluding[T any](quoter Quoter, exclude ...string) string {
+	cols := (&pop.Model{Value: new(T)}).Columns()
+	for _, e := range exclude {
+		cols.Remove(e)
+	}
+	return cols.QuotedString(quoter)
+}
+
+type (
+	PrefixQuoter struct {
+		Prefix string
+		Quoter Quoter
+	}
+	Quoter interface {
+		Quote(key string) string
+	}
+)
+
+func (pq *PrefixQuoter) Quote(key string) string {
+	return pq.Quoter.Quote(pq.Prefix + key)
+}

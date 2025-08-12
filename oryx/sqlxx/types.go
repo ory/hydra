a398b6444bff9476b2768e9e5f840bcf73fff3a6
Henning Perl <henning.perl@ory.sh>
2025-08-12 11:25:38 +0200

    feat(changelog-oel): choose identity schema in self-service registration and login flows
    GitOrigin-RevId: 8d6ee03cc8181d3277100a4b7412a3a113799964

--- oryx/sqlxx/types.go
+++ oryx/sqlxx/types.go
@@ -559,16 +559,16 @@ func (ns *NullDuration) UnmarshalJSON(data []byte) error {
 	return nil
 }
 
-func (ns Duration) IsZero() bool                { return time.Duration(ns) == 0 }
+func (ns Duration) IsZero() bool                { return ns == 0 }
 func (m StringSliceJSONFormat) IsZero() bool    { return len(m) == 0 }
 func (n StringSlicePipeDelimiter) IsZero() bool { return len(n) == 0 }
-func (ns NullBool) IsZero() bool                { return !ns.Valid }
-func (ns FalsyNullBool) IsZero() bool           { return !ns.Valid }
+func (ns NullBool) IsZero() bool                { return !ns.Valid || !ns.Bool }
+func (ns FalsyNullBool) IsZero() bool           { return !ns.Valid || !ns.Bool }
 func (ns NullString) IsZero() bool              { return len(ns) == 0 }
 func (ns NullTime) IsZero() bool                { return time.Time(ns).IsZero() }
 func (n MapStringInterface) IsZero() bool       { return len(n) == 0 }
 func (m JSONArrayRawMessage) IsZero() bool      { return len(m) == 0 || string(m) == "[]" }
 func (m JSONRawMessage) IsZero() bool           { return len(m) == 0 || string(m) == "null" }
 func (m NullJSONRawMessage) IsZero() bool       { return len(m) == 0 || string(m) == "null" }
-func (ns NullInt64) IsZero() bool               { return !ns.Valid }
-func (ns NullDuration) IsZero() bool            { return !ns.Valid }
+func (ns NullInt64) IsZero() bool               { return !ns.Valid || ns.Int == 0 }
+func (ns NullDuration) IsZero() bool            { return !ns.Valid || ns.Duration == 0 }

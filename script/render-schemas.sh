0a6ea5b70a4fa4508bd2bfde9c810d38695c3f2a
shaunn <shaunnkhan@users.noreply.github.com>
2025-09-09 13:38:36 -0700

    fix: use git hash to render ory x schema references
    GitOrigin-RevId: e07209f10a002acbb0df2f66f6f8746279032733

--- script/render-schemas.sh
+++ script/render-schemas.sh
@@ -2,8 +2,8 @@
 
 set -euxo pipefail
 
-ory_x_version="$(go list -f '{{.Version}}' -m github.com/ory/x)"
+schema_version="$(git rev-parse --short HEAD)"
 
-sed "s!ory://tracing-config!https://raw.githubusercontent.com/ory/x/$ory_x_version/otelx/config.schema.json!g;" spec/config.json > .schema/config.schema.json
+sed "s!ory://tracing-config!https://raw.githubusercontent.com/ory/hydra/$schema_version/oryx/otelx/config.schema.json!g;" spec/config.json > .schema/config.schema.json
 
 git commit --author="ory-bot <60093411+ory-bot@users.noreply.github.com>" -m "autogen: render config schema" .schema/config.schema.json || true
